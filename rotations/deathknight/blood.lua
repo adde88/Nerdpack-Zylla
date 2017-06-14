e047979d732685e8e12dbb4c971c8683dd5617bb
Andreas <adde88@gmail.com>
2017-06-14 22:02:09 +0200

    Optimized Blood-DK Routine.

--- rotations/deathknight/blood.lua
+++ rotations/deathknight/blood.lua
@@ -7,8 +7,9 @@ local GUI = {
 	{type = 'text', 	text = 'Left Alt: ', align = 'center'},
 	{type = 'text', 	text = 'Right Alt: ', align = 'center'},
 	{type = 'checkbox', text = 'Pause Enabled', key = 'kPause', default = true},
-	{type = 'checkbox', text = 'Use Trinket #1', key = 'kT1', default = false},
-	{type = 'checkbox', text = 'Use Trinket #2', key = 'kT2', default = false},
+	{type = 'checkbox', text = 'Use Trinket #1', key = 'kT1', default = true},
+	{type = 'checkbox', text = 'Use Trinket #2', key = 'kT2', default = true},
+	{type = 'checkbox', text = 'Ring of Collapsing Futures', key = 'kRoCF', default = true}
 } 
 
 local exeOnLoad = function()
@@ -26,13 +27,19 @@ local Util = {
 	{'%pause' , 'player.debuff(200904)||player.debuff(Sapped Soul)'}, -- Vault of the Wardens, Sapped Soul
 }
 
-local PreCombat = {
+local Trinkets = {
+	{'#trinket1', 'UI(kT1)'},
+	{'#trinket2', 'UI(kT2)'},
+	{'#Ring of Collapsing Futures', 'equipped(142173)&!player.debuff(Temptation)&UI(kRoCF)', 'target.enemy'},
+}
 
+local PreCombat = {
 }
 
 local Survival = {
-	{'Death Strike', 'player.health<=80&player.buff(Dark Succor)'},
 	{'Icebound Fortitude', 'player.health<=25||player.incdmg(2.5)>player.health.max*0.50||player.state.stun'},
+	{'Anti-Magic Shell', 'player.incdmg(2.5).magic>player.health.max*0.50'},
+	{'Wraith Walk', 'player.state.root'},
 }
 
 local Keybinds = {
@@ -41,30 +48,38 @@ local Keybinds = {
 	{'Death and Decay', 'keybind(lcontrol)', 'cursor.ground'},
 }
 
+local Cooldowns = {
+	{'Dancing Rune Weapon', 'target.inFront&target.inMelee&{player.incdmg(2.5)>player.health.max*0.50}||{player.health<=20}'},
+	{'Vampiric Blood', 'player.incdmg(2.5)>player.health.max*0.50||player.health<=25'},
+}
+
 local Interrupts = {
-	{'Mind Freeze'},
+	{'Mind Freeze', 'target.inFront&target.inMelee'},
 	{'Asphyxiate', 'target.range<=20&cooldown(Mind Freeze).remains>gcd&!prev_gcd(Mind Freeze)'},
 	{'Arcane Torrent', 'target.inMelee&cooldown(Mind Freeze).remains>gcd&!prev_gcd(Mind Freeze)'},
 }
 
 local xCombat = {
+	{'Death Grip', '!target.inMelee&target.range<=30&target.threat>99'},
+	{'Death\'s Caress', 'target.range<=30&{!target.debuff(Blood Plague)}||{target.debuff(Blood Plague).remains<=2}'},
 	{'Marrowrend', 'player.buff(Bone Shield).duration<=3&target.inFront&target.inMelee'},
-	{'Blood Boil', '!target.debuff(Blood Plague)'},
-	-- DnD here w/ crimson scourge proc
-	{'Death and Decay', 'player.buff(Crimson Scourge)&talent(2,1)&target.range<=30', 'player.ground'},
+	{'Marrowrend', 'player.buff(Bone Shield).count<=6&talent(3,1)&target.inFront&target.inMelee'},
+	{'Blood Boil', '!target.debuff(Blood Plague)&target.range<=10'},
+	{'Death and Decay', 'target.range<=30&{talent(2,1)&player.buff(Crimson Scourge)}||{player.area(10).enemies>1&player.buff(Crimson Scourge}', 'player.ground'},
+	{'Death and Decay', 'target.range<=30&{talent(2,1)&player.runes>=3}||{player.area(10).enemies>=3', 'player.ground'},
+	{'Death and Decay', '!talent(2,1)&target.range<=30&player.area(10).enemies=1&player.buff(Crimson Scourge)', 'player.ground'},
 	{'Death Strike', 'player.runicpower>=75&target.inFront&target.inMelee'},
-	{'Marrowrend', 'player.buff(Bone Shield).count<=6&target.inFront&target.inMelee'},
-	-- DnD here 3 or more runes and usiwwwwng talent
 	{'Heart Strike', 'player.runes>=3&target.inFront&target.inMelee'},
 	{'Consumption', 'target.inFront&target.inMelee'},
-	{'Blood Boil'}, 
 }
 
 local inCombat = {
 	{Util},
 	{Keybinds},
-	{Interrupts, 'target.interruptAt(50)&toggle(Interrupts)&target.inFront&target.inMelee'},
+	{Trinkets},
+	{Interrupts, 'target.interruptAt(80)&toggle(Interrupts)'},
 	{Survival},
+	{Cooldowns, 'toggle(Cooldowns)'},
 	{xCombat},
 }
 
