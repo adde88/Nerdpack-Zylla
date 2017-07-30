8a9a69255244e80b1236da6fd0c4ae7ff2814084
Andreas <adde88@gmail.com>
2017-07-30 21:52:24 +0200

    Re-written big parts of the routine, it should work.

--- rotations/mage/fire.lua
+++ rotations/mage/fire.lua
@@ -62,7 +62,7 @@ local Interrupts = {
 local Cooldowns = {
 	--# Executed every time the actor is available.
 	{'Time Warp', 'toggle(xTimeWarp)&{{xtime==0&!player.buff(Bloodlust)}||{!player.buff(Bloodlust)&equipped(132410)}}'},
-	{'#trinket2', 'target.range<50&target.area(10).enemies>3'},
+	{'#trinket2', 'target.range<41&target.area(10).enemies>3'},
 }
 
 local Survival = {
@@ -71,12 +71,12 @@ local Survival = {
 }
 
 local Talents = {
-	{'Flame On', 'talent(4,2)&{action(Fire Blast).charges<0.2&{cooldown(Combustion).remains>65||target.time_to_die<cooldown(Combustion).remains}}'},
-	{'Blast Wave', 'talent(4,1)&{{!player.buff(Combustion)}||{player.buff(Combustion)&action(Fire Blast).charges<1&action(Phoenix\'s Flames).charges<1}}'},
-	{'Meteor', 'talent(7,3)&{cooldown(Combustion).remains>30||{cooldown(Combustion).remains>target.time_to_die}||player.buff(Rune of Power)}'},
-	{'Cinderstorm', 'talent(7,2)&{cooldown(Combustion).remains<action(Cinderstorm).cast_time&{player.buff(Rune of Power)||!talent(3,2)}||cooldown(Combustion).remains>10*spell_haste&!player.buff(Combustion)}'},
+	{'Flame On', 'action(Fire Blast).charges<0.2&{player.spell(Combustion).cooldown>65||target.time_to_die<player.spell(Combustion).cooldown}'},
+	{'Blast Wave', '!player.buff(Combustion)||{player.buff(Combustion)&action(Fire Blast).charges<1&action(Phoenix\'s Flames).charges<1}'},
+	{'Meteor', 'player.spell(Combustion).cooldown>30||{{player.spell(Combustion).cooldown>target.time_to_die}||player.buff(Rune of Power)}'},
+	{'Cinderstorm', 'player.spell(Combustion).cooldown<action(Cinderstorm).cast_time&{{player.buff(Rune of Power)||!talent(3,2)}||player.spell(Combustion).cooldown>10*spell_haste&!player.buff(Combustion)}'},
 	{'Dragon\'s Breath', 'equipped(132863)'},
-	{'Living Bomb', 'talent(6,1)&target.area(10).enemies>1&!player.buff(Combustion)'}
+	{'Living Bomb', 'target.area(10).enemies>1&!player.buff(Combustion)'}
 }
 
 local Combustion = {
@@ -89,14 +89,14 @@ local Combustion = {
 	--Want to cast Flames if we have 3 charges so we can recharges going
 	{'Phoenix\'s Flames', 'action(Phoenix\'s Flames).charges>2.7&player.buff(Combustion)&!player.buff(Hot Streak!)'},
 	{'&Fire Blast', 'player.buff(Heating Up)&!player.lastcast(Fire Blast)&player.buff(Combustion)'},
-	{'Phoenix\'s Flames', 'artifact(Phoenix\'s Flames).enabled'},
-	{'Scorch', 'player.buff(Combustion).remains>action(Scorch).cast_time'},
+	{'Phoenix\'s Flames'},
+	{'Scorch', 'player.buff(Combustion).duration>action(Scorch).cast_time'},
 	{'Scorch'}
 }
 
 local RoP = {
 	{'Rune of Power'},
-	{'Pyroblast', 'player.buff(Hot Streak!)'},
+	{'&Pyroblast', 'player.buff(Hot Streak!)'},
 	{Talents},
 	{'Pyroblast', 'player.buff(Kael\'thas\'s Ultimate Ability)'},
 	{'&Fire Blast', 'player.buff(Heating Up)&!player.lastcast(Fire Blast)&!player.casting(Rune of Power)&!player.lastcast(Phoenix\'s Flames)'},
@@ -106,30 +106,30 @@ local RoP = {
 }
 
 local MainRotation = {
-	{'Pyroblast', 'player.buff(Hot Streak!)&player.buff(Hot Streak!).remains<action(Fireball).execute_time'},
-	{'Phoenix\'s Flames', 'artifact(Phoenix\'s Flames).enabled&{action(Phoenix\'s Flames).charges>2.7&target.area(8).enemies>2}'},
+	{'Pyroblast', 'player.buff(Hot Streak!)&player.buff(Hot Streak!).duration<action(Fireball).execute_time'},
+	{'Phoenix\'s Flames', 'action(Phoenix\'s Flames).charges>2.7&target.area(8).enemies>2'},
 	{'Flamestrike', 'talent(6,3)&target.area(10).enemies>2&player.buff(Hot Streak!)', 'target.ground'},
 	{'&Pyroblast', 'player.buff(Hot Streak!)&!player.lastgcd(Pyroblast)&{player.casting(Fireball).percent>90||player.lastcast(Fireball)}'},
-	{'Pyroblast', 'player.buff(Hot Streak!)&target.health<35&equipped(132454)'},
+	{'&Pyroblast', 'player.buff(Hot Streak!)&target.health<35&equipped(132454)'},
 	{'Pyroblast', 'player.buff(Kael\'thas\'s Ultimate Ability)'},
 	{Talents},
-	--{'&Fire Blast', 'player.buff(Heating Up)&!prev_off_gcd(Fire Blast)&action(Fire Blast).charges>0&cooldown(Combustion).remains<action(Fire Blast).cooldown_to_max'},
-	{'&Fire Blast', 'player.casting(Fireball).percent>40&xtime>3&!player.casting(Rune of Power)&!talent(7,1)&player.buff(Heating Up)&!player.lastcast(Fire Blast)&{!talent(3,2)||action(Fire Blast).charges>1.4||cooldown(Combustion).remains<40}&(3-action(Fire Blast).charges)*(12*spell_haste)<=cooldown(Combustion).remains+3'},
-	{'&Fire Blast', 'player.casting(Fireball).percent>40&xtime>3&!player.casting(Rune of Power)&talent(7,1)&player.buff(Heating Up)&!player.lastcast(Fire Blast)&{!talent(3,2)||action(Fire Blast).charges>1.5||cooldown(Combustion).remains<40}&{3-action(Fire Blast).charges}*{18*spell_haste}<=cooldown(Combustion).remains+3'},
-	{'Phoenix\'s Flames', '{player.buff(Combustion)||player.buff(Rune of Power)||player.buff(Incanter\'s Flow).stack>3||talent(3,1)}&{4-action(Phoenix\'s Flames).charges}*13<cooldown(Combustion).remains+5||target.time_to_die<10'},
-	{'Phoenix\'s Flames', '{player.buff(Combustion)||player.buff(Rune of Power)}&{4-action(Phoenix\'s Flames).charges}*30<cooldown(Combustion).remains+5'},
+	--{'&Fire Blast', 'player.buff(Heating Up)&!prev_off_gcd(Fire Blast)&action(Fire Blast).charges>0&player.spell(Combustion).cooldown<action(Fire Blast).cooldown_to_max'},
+	{'&Fire Blast', 'player.casting(Fireball).percent>40&xtime>3&!player.casting(Rune of Power)&!talent(7,1)&player.buff(Heating Up)&!player.lastcast(Fire Blast)&{!talent(3,2)||action(Fire Blast).charges>1.4||player.spell(Combustion).cooldown<40}&(3-action(Fire Blast).charges)*(12*spell_haste)<=player.spell(Combustion).cooldown+3'},
+	{'&Fire Blast', 'player.casting(Fireball).percent>40&xtime>3&!player.casting(Rune of Power)&talent(7,1)&player.buff(Heating Up)&!player.lastcast(Fire Blast)&{!talent(3,2)||action(Fire Blast).charges>1.5||player.spell(Combustion).cooldown<40}&{3-action(Fire Blast).charges}*{18*spell_haste}<=player.spell(Combustion).cooldown+3'},
+	{'Phoenix\'s Flames', '{player.buff(Combustion)||player.buff(Rune of Power)||player.buff(Incanter\'s Flow).stack>3||talent(3,1)}&{{4-action(Phoenix\'s Flames).charges}*13<player.spell(Combustion).cooldown+5||target.time_to_die<10}'},
+	{'Phoenix\'s Flames', '{player.buff(Combustion)||player.buff(Rune of Power)}&{4-action(Phoenix\'s Flames).charges}*30<player.spell(Combustion).cooldown+5'},
 	{'Scorch', 'target.health<35&equipped(132454)'},
-	{'Ice Floes', 'cooldown(61304).remains<0.5&xmoving==1&!player.lastcast(Ice Floes)&!player.buff(Ice Floes)'},
-	{'Fireball', 'xmoving==0||player.buff(Ice Floes)'},
+	{'Ice Floes', 'player.spell(61304).cooldown<0.5&player.moving&!player.lastcast(Ice Floes)&!player.buff(Ice Floes)'},
+	{'Fireball', '!player.moving||player.buff(Ice Floes)'},
 	{'Ice Barrier', '!player.buff(Ice Barrier)&!player.buff(Combustion)&!player.buff(Rune of Power)'},
-	{'Scorch', 'xmoving==1&!player.buff(Ice Floes)'},
+	{'Scorch', 'player.moving&!player.buff(Ice Floes)'},
 	{'Dragon\'s Breath', 'target.range<22&player.area(10).enemies>1'},
 }
 
 local xCombat = {
-	{'Rune of Power', 'xmoving==0&toggle(cooldowns)&{cooldown(Combustion).remains>40}&{!player.buff(Combustion)&{cooldown(Flame On).remains<5||cooldown(Flame On).remains>30}&!talent(7,1)||target.time_to_die<11||talent(7,1)&{action(Rune of Power).charges>1.8||xtime<40}&{cooldown(Combustion).remains>40)}}'},
-	{Combustion, 'xmoving==0&{cooldown(Combustion).remains<=action(Rune of Power).cast_time+gcd||player.buff(Combustion)}'},
-	{RoP, 'xmoving==0&player.buff(Rune of Power)&!player.buff(Combustion)'},
+	{'Rune of Power', '!player.moving&toggle(cooldowns)&{{player.spell(Combustion).cooldown>40}&{!player.buff(Combustion)&{player.spell(Flame On).cooldown<5||player.spell(Flame On).cooldown>30}&!talent(7,1)||target.time_to_die<11||talent(7,1)&{action(Rune of Power).charges>1.8||xtime<40}&{player.spell(Combustion).cooldown>40)}}}'},
+	{Combustion, '!player.moving&{player.spell(Combustion).cooldown<=action(Rune of Power).cast_time+gcd||player.buff(Combustion)}'},
+	{RoP, '!player.moving&player.buff(Rune of Power)&!player.buff(Combustion)'},
 	{MainRotation, '!player.casting(Rune of Power)'},
 }
 
@@ -138,10 +138,10 @@ local inCombat = {
 	{Trinkets},
 	{Heirlooms},
 	{Keybinds},
-	{Interrupts, 'target.interruptAt(70)&toggle(interrupts)&target.inFront&target.range<50'},
+	{Interrupts, 'target.interruptAt(70)&toggle(interrupts)&target.inFront&target.range<41'},
 	{Cooldowns, 'toggle(cooldowns)'},
 	{Survival, 'player.health<100'},
-	{xCombat, 'target.range<50&target.inFront'},
+	{xCombat, 'target.range<41&target.inFront'},
 }
 
 local outCombat = {
