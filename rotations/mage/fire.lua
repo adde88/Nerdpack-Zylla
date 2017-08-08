bb0a5091683b16f0e9bd921346ad91e0cc9b7ae3
Andreas <adde88@gmail.com>
2017-08-08 22:12:09 +0200

    Added Random Interupts, with Counterspell, RoF, and Polymorph. (toggle on/off in settings). + some cleanup

--- rotations/mage/fire.lua
+++ rotations/mage/fire.lua
@@ -5,6 +5,9 @@ local Trinkets = _G['Zylla.Trinkets']
 local Heirlooms = _G['Zylla.Heirlooms']
 
 local GUI = {
+	--Logo
+  {type = "texture", texture = "Interface\\AddOns\\Nerdpack-Zylla\\media\\logo.blp", width = 128, height = 128, offset = 90, y = 42, center = true},
+  {type = 'ruler'},	  {type = 'spacer'},
 	-- Keybinds
 	{type = 'header', 	text = 'Keybinds', align = 'center'},
 	{type = 'text', 	text = 'Left Shift: Pause', align = 'center'},
@@ -15,11 +18,13 @@ local GUI = {
 	-- Settings
 	{type = 'header', 	text = 'Class Settings', align = 'center'},
 	{type = 'checkbox', text = 'Pause Enabled', key = 'kPause', default = true},
+	{type = 'checkbox', text = 'Ring of Frost (Interrupt)',	key = 'RoF_Int',	default = true},
+	{type = 'checkbox', text = 'Polymorph (Interrupt)',	key = 'Pol_Int',	default = false},
 	{type = 'ruler'},	{type = 'spacer'},
 	-- Trinkets + Heirlooms for leveling
 	{type = 'header', 	text = 'Trinkets/Heirlooms', align = 'center'},
-	{type = 'checkbox', text = 'Use Trinket #1', key = 'kT1', default = true},
-	{type = 'checkbox', text = 'Use Trinket #2', key = 'kT2', default = true},
+	--{type = 'checkbox', text = 'Use Trinket #1', key = 'kT1', default = true},
+	--{type = 'checkbox', text = 'Use Trinket #2', key = 'kT2', default = true},
 	{type = 'checkbox', text = 'Ring of Collapsing Futures', key = 'kRoCF', default = true},
 	{type = 'checkbox', text = 'Use Heirloom Necks When Below X% HP', key = 'k_HEIR', default = true},
 	{type = 'spinner',	text = '', key = 'k_HeirHP', default = 40},
@@ -34,6 +39,7 @@ local exeOnLoad = function()
 	print('|cffADFF2F --- |rMage |cffADFF2FFire |r')
 	print('|cffADFF2F --- |rRecommended Talents: 1/1 - 2/X - 3/2 - 4/2 - 5/X - 6/2 - 7/1')
 	print('|cffADFF2F ----------------------------------------------------------------------|r')
+  print('|cffFFFB2F Configuration: |rRight-click MasterToggle and go to Combat Routines Settings!|r')
 
 	NeP.Interface:AddToggle({
 		key = 'xTimeWarp',
@@ -41,6 +47,13 @@ local exeOnLoad = function()
 		text = 'Automatically use Time Warp.',
 		icon = 'Interface\\Icons\\ability_mage_timewarp',
 	})
+
+	NeP.Interface:AddToggle({
+	 key = 'xIntRandom',
+	 name = 'Interrupt Anyone',
+	 text = 'Interrupt all nearby enemies, without targeting them.',
+	 icon = 'Interface\\Icons\\inv_ammo_arrow_04',
+ })
 
 end
 
@@ -51,40 +64,45 @@ local Keybinds = {
 }
 
 local PreCombat = {
-	{'Blazing Barrier' , '!player.buff(Blazing Barrier)&player.area(40).enemies>0'},
+	{'Blazing Barrier' , '!player.buff(Blazing Barrier)&player.area(50).enemies>0'},
 }
 
 local Interrupts = {
 	{'!Counterspell'},
-	{'!Arcane Torrent', 'target.inMelee&spell(Counterspell).cooldown>gcd&!player.lastcast(Counterspell)'}
+	{'!Arcane Torrent', 'target.inMelee&spell(Counterspell).cooldown>gcd&!player.lastcast(Counterspell)'},
+	{"!Dragon's Breath", 'player.spell(Counterspell).cooldown>gcd&!prev_gcd(Counterspell)&player.area(12).enemies.infront>0', 'enemies'},
+}
+
+local Interrupts_Random = {
+	{'!Counterspell', 'interruptAt(70)&toggle(xIntRandom)&toggle(Interrupts)&inFront&range<41', 'enemies'},
+  {"!Dragon's Breath", 'interruptAt(70)&toggle(xIntRandom)&toggle(Interrupts)&player.spell(Counterspell).cooldown>gcd&!prev_gcd(Counterspell)&player.area(12).enemies.infront>0&range<13', 'enemies'},
+	{'!Ring of Frost', 'advanced&!player.moving&UI(RoF_Int)&interruptAt(1)&toggle(xIntRandom)&toggle(Interrupts)&player.spell(Counterspell).cooldown>gcd&!prev_gcd(Counterspell)&range<31', 'enemies.ground'},
+	{'!Polymorph', '!player.moving&UI(Pol_Int)&interruptAt(1)&toggle(xIntRandom)&toggle(Interrupts)&player.spell(Counterspell).cooldown>gcd&!prev_gcd(Counterspell)&range<31', 'enemies'},
 }
 
 local Cooldowns = {
-	--# Executed every time the actor is available.
-	{'Time Warp', 'toggle(xTimeWarp)&{{xtime==0&!player.buff(Bloodlust)}||{!player.buff(Bloodlust)&equipped(132410)}}'},
-	{'#trinket2', 'target.range<41&target.area(10).enemies>3'},
+	{'Blood Fury'},
+	{'Berserking', 'toggle(xTimeWarp)'},
+	{'Time Warp', 'toggle(xTimeWarp)'},
+	{'#Trinket1', 'target.range<41&target.area(10).enemies>=3&equipped(144259)'}, -- Kil'jaeden's Burning Wish / AoE Trinket
 }
 
 local Survival = {
 	{'!Ice Block', 'player.health<30||player.debuff(Cauterize)'},
-	{'Blazing Barrier' , '!player.buff(Blazing Barrier)'},
+	{'Blazing Barrier' , 'player.buff(Blazing Barrier).duration<gcd'},
 }
 
 local Talents = {
-	{'Flame On', 'action(Fire Blast).charges<0.2&{player.spell(Combustion).cooldown>65||target.time_to_die<player.spell(Combustion).cooldown}'},
+	{'Flame On', 'action(Fire Blast).charges<0.2&{player.spell(Combustion).cooldown>65||target.ttd<player.spell(Combustion).cooldown}'},
 	{'Blast Wave', '!player.buff(Combustion)||{player.buff(Combustion)&action(Fire Blast).charges<1&action(Phoenix\'s Flames).charges<1}'},
-	{'Meteor', 'player.spell(Combustion).cooldown>30||{{player.spell(Combustion).cooldown>target.time_to_die}||player.buff(Rune of Power)}'},
+	{'Meteor', 'player.spell(Combustion).cooldown>30||{{player.spell(Combustion).cooldown>target.ttd}||player.buff(Rune of Power)}'},
 	{'Cinderstorm', 'player.spell(Combustion).cooldown<action(Cinderstorm).cast_time&{{player.buff(Rune of Power)||!talent(3,2)}||player.spell(Combustion).cooldown>10*spell_haste&!player.buff(Combustion)}'},
 	{'Dragon\'s Breath', 'equipped(132863)'},
-	{'Living Bomb', 'target.area(10).enemies>1&!player.buff(Combustion)'}
+	{'Living Bomb', '!target.debuff(Living Bomb)&target.area(10).enemies>1&!player.buff(Combustion)'}
 }
 
 local Combustion = {
 	{'Rune of Power', '!player.buff(Combustion)'},
-	{Talents},
-	{'&Combustion', 'player.buff(Rune of Power)||player.casting(Rune of Power).percent>80'},
-	{'Blood Fury'},
-	{'Berserking'},
 	{'&Pyroblast', 'player.buff(Hot Streak!)&player.buff(Combustion)'},
 	--Want to cast Flames if we have 3 charges so we can recharges going
 	{'Phoenix\'s Flames', 'action(Phoenix\'s Flames).charges>2.7&player.buff(Combustion)&!player.buff(Hot Streak!)'},
@@ -97,7 +115,6 @@ local Combustion = {
 local RoP = {
 	{'Rune of Power'},
 	{'&Pyroblast', 'player.buff(Hot Streak!)'},
-	{Talents},
 	{'Pyroblast', 'player.buff(Kael\'thas\'s Ultimate Ability)'},
 	{'&Fire Blast', 'player.buff(Heating Up)&!player.lastcast(Fire Blast)&!player.casting(Rune of Power)&!player.lastcast(Phoenix\'s Flames)'},
 	{'Phoenix\'s Flames', '!player.lastgcd(Phoenix\'s Flames)'},
@@ -108,15 +125,15 @@ local RoP = {
 local MainRotation = {
 	{'Pyroblast', 'player.buff(Hot Streak!)&player.buff(Hot Streak!).duration<action(Fireball).execute_time'},
 	{'Phoenix\'s Flames', 'action(Phoenix\'s Flames).charges>2.7&target.area(8).enemies>2'},
-	{'Flamestrike', 'talent(6,3)&target.area(10).enemies>2&player.buff(Hot Streak!)', 'target.ground'},
+	{'&Flamestrike', 'advanced&target.range<41&talent(6,3)&target.area(10).enemies>2&player.buff(Hot Streak!)', 'target.ground'},
+	{'&Flamestrike', '!advanced&target.range<41&talent(6,3)&player.area(10).enemies>2&player.buff(Hot Streak!)', 'player.ground'},
 	{'&Pyroblast', 'player.buff(Hot Streak!)&!player.lastgcd(Pyroblast)&{player.casting(Fireball).percent>90||player.lastcast(Fireball)}'},
 	{'&Pyroblast', 'player.buff(Hot Streak!)&target.health<35&equipped(132454)'},
 	{'Pyroblast', 'player.buff(Kael\'thas\'s Ultimate Ability)'},
-	{Talents},
 	--{'&Fire Blast', 'player.buff(Heating Up)&!prev_off_gcd(Fire Blast)&action(Fire Blast).charges>0&player.spell(Combustion).cooldown<action(Fire Blast).cooldown_to_max'},
 	{'&Fire Blast', 'player.casting(Fireball).percent>40&xtime>3&!player.casting(Rune of Power)&!talent(7,1)&player.buff(Heating Up)&!player.lastcast(Fire Blast)&{!talent(3,2)||action(Fire Blast).charges>1.4||player.spell(Combustion).cooldown<40}&(3-action(Fire Blast).charges)*(12*spell_haste)<=player.spell(Combustion).cooldown+3'},
 	{'&Fire Blast', 'player.casting(Fireball).percent>40&xtime>3&!player.casting(Rune of Power)&talent(7,1)&player.buff(Heating Up)&!player.lastcast(Fire Blast)&{!talent(3,2)||action(Fire Blast).charges>1.5||player.spell(Combustion).cooldown<40}&{3-action(Fire Blast).charges}*{18*spell_haste}<=player.spell(Combustion).cooldown+3'},
-	{'Phoenix\'s Flames', '{player.buff(Combustion)||player.buff(Rune of Power)||player.buff(Incanter\'s Flow).stack>3||talent(3,1)}&{{4-action(Phoenix\'s Flames).charges}*13<player.spell(Combustion).cooldown+5||target.time_to_die<10}'},
+	{'Phoenix\'s Flames', '{player.buff(Combustion)||player.buff(Rune of Power)||player.buff(Incanter\'s Flow).stack>3||talent(3,1)}&{{4-action(Phoenix\'s Flames).charges}*13<player.spell(Combustion).cooldown+5||target.ttd<10}'},
 	{'Phoenix\'s Flames', '{player.buff(Combustion)||player.buff(Rune of Power)}&{4-action(Phoenix\'s Flames).charges}*30<player.spell(Combustion).cooldown+5'},
 	{'Scorch', 'target.health<35&equipped(132454)'},
 	{'Ice Floes', 'player.spell(61304).cooldown<0.5&player.moving&!player.lastcast(Ice Floes)&!player.buff(Ice Floes)'},
@@ -127,26 +144,31 @@ local MainRotation = {
 }
 
 local xCombat = {
-	{'Rune of Power', '!player.moving&toggle(cooldowns)&{{player.spell(Combustion).cooldown>40}&{!player.buff(Combustion)&{player.spell(Flame On).cooldown<5||player.spell(Flame On).cooldown>30}&!talent(7,1)||target.time_to_die<11||talent(7,1)&{action(Rune of Power).charges>1.8||xtime<40}&{player.spell(Combustion).cooldown>40)}}}'},
+	{'Rune of Power', '!player.moving&toggle(cooldowns)&{{player.spell(Combustion).cooldown>40}&{!player.buff(Combustion)&{player.spell(Flame On).cooldown<5||player.spell(Flame On).cooldown>30}&!talent(7,1)||target.ttd<11||talent(7,1)&{action(Rune of Power).charges>1.8||xtime<40}&{player.spell(Combustion).cooldown>40)}}}'},
 	{Combustion, '!player.moving&{player.spell(Combustion).cooldown<=action(Rune of Power).cast_time+gcd||player.buff(Combustion)}'},
 	{RoP, '!player.moving&player.buff(Rune of Power)&!player.buff(Combustion)'},
-	{MainRotation, '!player.casting(Rune of Power)'},
+	{MainRotation},
 }
 
 local inCombat = {
 	{Util},
-	{Trinkets},
-	{Heirlooms},
-	{Keybinds},
-	{Interrupts, 'target.interruptAt(70)&toggle(interrupts)&target.inFront&target.range<41'},
-	{Cooldowns, 'toggle(cooldowns)'},
-	{Survival, 'player.health<100'},
-	{xCombat, 'target.range<41&target.inFront'},
+	{Trinkets, '!player.casting(Rune of Power)'},
+	{Heirlooms, '!player.casting(Rune of Power)'},
+	{Keybinds, '!player.casting(Rune of Power)'},
+	{Interrupts_Random, '!player.casting(Rune of Power)'},
+	{Interrupts, 'target.interruptAt(70)&toggle(interrupts)&target.inFront&target.range<41&!player.casting(Rune of Power)'},
+	{Cooldowns, 'toggle(cooldowns)&!player.casting(Rune of Power)'},
+	{Survival, '!player.casting(Rune of Power)'},
+	{Talents, '!player.casting(Rune of Power)'},
+	{xCombat, 'target.range<41&target.inFront&!player.casting(Rune of Power)'},
+	{'&Combustion', 'toggle(cooldowns)&target.range<41&target.ttd>12&{player.buff(Rune of Power)||player.casting(Rune of Power).percent>80}'},
 }
 
 local outCombat = {
 	{Keybinds},
-	{PreCombat}
+	{PreCombat},
+	{Interrupts_Random},
+	{Interrupts, 'target.interruptAt(70)&toggle(interrupts)&target.inFront&target.range<41'},
 }
 
 NeP.CR:Add(63, {
