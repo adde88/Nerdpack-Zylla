3ea37db8e61d48b009e16a0f93cb9ec1bbaa35d3
Andreas <adde88@gmail.com>
2017-08-12 22:26:56 +0200

    This fixes Crash Lightning not being used, and should perform better overall.

--- rotations/shaman/enhancement.lua
+++ rotations/shaman/enhancement.lua
@@ -1,13 +1,5 @@
 local _, Zylla = ...
 
---[[
-The Following routine is based on an article found on Wowhead.com
-http://www.wowhead.com/enhancement-shaman-rotation-guide
-
-I'm trying to follow it as precise as possible.
-	- Zylla
-]]
-
 local Util = _G['Zylla.Util']
 local Trinkets = _G['Zylla.Trinkets']
 local Heirlooms = _G['Zylla.Heirlooms']
@@ -27,25 +19,27 @@ local GUI = {
 	-- Survival
 	{type = 'header', 	text = 'Survival',									  	      align = 'center'},
 	{type = 'checkbox', text = 'Enable Healing Surge',								key = 'E_HS',           default = false},
-	{type = 'spinner', 	text = 'Healing Surge below HP%',             key = 'HS_HP',          default = 50},
+	{type = 'spinner', 	text = 'Healing Surge below HP%',             key = 'HS_HP',          default = 20},
 	{type = 'spinner', 	text = 'Astral Shift below HP%',             	key = 'AS_HP',          default = 40},
 	{type = 'checkbox', text = 'Use Rainfall to Heal Player',					key = 'E_RF_PL',        default = true},
 	{type = 'spinner', 	text = 'below HP%',             							key = 'P_RF_HP',       	default = 33},
+	{type = 'spinner',	text = 'Healthstone',										      key = 'HS',						  default = 30},
+	{type = 'spinner',	text = 'Ancient Healing Potion',		 			    key = 'AHP',	  				default = 25},
 	{type = 'ruler'},	  {type = 'spacer'},
 	-- Group/Party stuff...
 	{type = 'header', 	text = 'Party/Group',									  	    align = 'center'},
 	{type = 'checkbox', text = 'Heal Lowest Party Member',						key = 'E_HEAL',        default = false},
 	{type = 'spinner', 	text = 'below HP%',             							key = 'L_HS_HP',       default = 33},
 	{type = 'checkbox', text = 'Use Rainfall to Heal Party',					key = 'E_HEAL_RF',     default = false},
-	{type = 'spinner', 	text = 'below HP%',             							key = 'L_RF_HP',       default = 33},
+	{type = 'spinner', 	text = 'below HP%',             							key = 'L_RF_HP',       default = 25},
 	{type = 'ruler'},	  {type = 'spacer'},
 	-- Trinkets + Heirlooms for leveling
-	{type = 'header', 	text = 'Trinkets/Heirlooms', align = 'center'},
-	{type = 'checkbox', text = 'Use Trinket #1', key = 'kT1', default = true},
-	{type = 'checkbox', text = 'Use Trinket #2', key = 'kT2', default = true},
-	{type = 'checkbox', text = 'Ring of Collapsing Futures', key = 'kRoCF', default = true},
-	{type = 'checkbox', text = 'Use Heirloom Necks When Below X% HP', key = 'k_HEIR', default = true},
-	{type = 'spinner',	text = '', key = 'k_HeirHP', default = 40},
+	{type = 'header', 	text = 'Trinkets/Heirlooms', 									align = 'center'},
+	{type = 'checkbox', text = 'Use Trinket #1', 											key = 'kT1', 						default = false},
+	{type = 'checkbox', text = 'Use Trinket #2', 											key = 'kT2',						default = false},
+	{type = 'checkbox', text = 'Ring of Collapsing Futures', 					key = 'kRoCF', 					default = false},
+	{type = 'checkbox', text = 'Use Heirloom Necks When Below X% HP', key = 'k_HEIR', 				default = false},
+	{type = 'spinner',	text = '', 																		key = 'k_HeirHP', 			default = 40},
 }
 
 local exeOnLoad = function()
@@ -56,14 +50,14 @@ local exeOnLoad = function()
 	print('|cffADFF2F --- |rShaman |cffADFF2FEnhancement (Default)|r')
 	print('|cffADFF2F --- |rRecommended Talents: 1/3 - 2/X - 3/X - 4/3 - 5/1 - 6/1 - 7/2')
 	print('|cffADFF2F ----------------------------------------------------------------------|r')
-	print('|cffFFFB2F Configuration: |rRight-click MasterToggle and go to Combat Routines Settings!|r')
+		print('|cffFFFB2F Configuration: |rRight-click MasterToggle and go to Combat Routines Settings!|r')
 
 	NeP.Interface:AddToggle({
-	 key = 'xIntRandom',
-	 name = 'Interrupt Anyone',
-	 text = 'Interrupt all nearby enemies, without targeting them. Might require advanced unlocker on some routines!',
-	 icon = 'Interface\\Icons\\inv_ammo_arrow_04',
- })
+		key = 'xIntRandom',
+		name = 'Interrupt Anyone',
+		text = 'Interrupt all nearby enemies, without targeting them. Might require advanced unlocker on some routines!',
+		icon = 'Interface\\Icons\\inv_ammo_arrow_04',
+	})
 
 end
 
@@ -83,11 +77,13 @@ local PreCombat = {
 
 local Survival = {
 	{'!Healing Surge', '!moving&UI(E_HS)&player.health<UI(HS_HP)&player.maelstrom>10', 'player'},
-	{'!Rainfall', 'UI(E_RF_PL)&player.health<UI(P_RF_HP)&player.maelstrom>10&range<41', 'player.ground'}
+	{'!Rainfall', 'UI(E_RF_PL)&player.health<UI(P_RF_HP)&player.maelstrom>10&range<41', 'player.ground'},
+	{'#127834', 'item(127834).count>0&player.health<UI(AHP)'},        -- Ancient Healing Potion
+  {'#5512', 'item(5512).count>0&player.health<UI(HS)', 'player'}	  --Health Stone
 }
 
 local Party = {
-	{'!Healing Surge', 'UI(E_HEAL)&health<UI(L_HS_HP)&player.maelstrom>10&range<41', 'lowest'},
+	{'!Healing Surge', '!moving&UI(E_HEAL)&health<UI(L_HS_HP)&player.maelstrom>10&range<41', 'lowest'},
 	{'!Rainfall', 'advanced&UI(E_HEAL_RF)&health<UI(L_RF_HP)&player.maelstrom>10&range<41', 'lowest.ground'}
 }
 
@@ -101,36 +97,34 @@ local Cooldowns = {
 }
 
 local Interrupts = {
-	{'!Wind Shear'}
+	{'!Wind Shear', 'target.inFront'},
+	{'!Lightning Surge Totem', 'advanced&player.spell(Wind Shear).cooldown>gcd&!player.lastgcd(Wind Shear)', 'target.ground'}
 }
 
 local Interrupts_Random = {
-	{'!Wind Shear', 'advanced&interruptAt(70)&toggle(xIntRandom)&toggle(Interrupts)&range<41', 'enemies'},
-	{'!Lightning Surge Totem', 'advanced&interruptAt(1)&toggle(xIntRandom)&toggle(Interrupts)&player.spell(Wind Shear).cooldown>gcd&!prev_gcd(Wind Shear)&inFront&range<41', 'enemies.ground'}
+	{'!Wind Shear', 'interruptAt(70)&toggle(xIntRandom)&toggle(Interrupts)&range<41', 'enemies'},
+	{'!Lightning Surge Totem', 'advanced&interruptAt(1)&toggle(xIntRandom)&toggle(Interrupts)&player.spell(Wind Shear).cooldown>gcd&!player.lastgcd(Wind Shear)&range<41', 'enemies.ground'}
 }
 
 local xCombat = {
-	{'Lightning Shield', '!player.buff(Lightning Shield)'},
-	{'Windstrike', 'player.spell(Ascendance).cooldown', 'target'},
-	{'Crash Lightning', '{player.area(8).enemies>=2&!player.buff(Crash Lightning)}||{set_bonus(t20)>=2&player.buff(Lightning Crash).duration<gcd}', 'target'},
-	{'Crash Lightning', 'lastgcd(Feral Spirit)||player.buff(Feral Spirit)', 'target'},
-	{'Stormstrike', '!talent(4,3)&player.area(8).enemies>2', 'target'},
-	{'Stormstrike', 'player.buff(Stormbringer)', 'target'},
-	{'Frostbrand', 'talent(4,3)&player.buff(Frostbrand).remains<4.5', 'target'},
-	{'Windsong', nil, 'target'},
-	{'Fury of Air', 'talent(6,2)&player.buff(Fury of Air).duration<gcd', 'target'},
-	{'Lightning Bolt', 'talent(5,2)&player.maelstrom>50', 'target'},
-	{'Flametongue', 'player.buff(Flametongue).remains<4.5', 'target'},
-	{'Lava Lash', 'talent(1,2)&player.buff(Hot Hand).react', 'target'},
-	{'Lava Lash', 'player.maelstrom>40', 'target'},
-	{'Rockbiter', '{player.buff(Landslide).duration<gcd}||{player.spell(Rockbiter).charges==1&player.cooldown(Rockbiter).recharge_time<gcd}', 'target'},
-	-- Fillers below... not exactly sure how to deal with them atm.
-	-- Some are to be casted when nothing else is to be casted. TODO:
-	{'Flametongue', nil, 'target'},
-	{'Earthen Spike', nil, 'target'},
-	{'Stormstrike', nil, 'target'},
-	{'Sundering', nil, 'target'},
-	{'Rockbiter', nil, 'target'}
+	{'Windstrike', 'player.buff(Ascendance)||lastcast(Ascendance)'},
+	{'Crash Lightning', '{toggle(AoE)&{player.area(8).enemies>=2||player.buff(Lightning Crash).duration<gcd}}||{!toggle(AoE)&player.buff(Lightning Crash).duration<gcd}'},
+	{'Crash Lightning', 'lastgcd(Feral Spirit)'},
+	{'Stormstrike', '!talent(4,3)&player.area(8).enemies>2'},
+	{'Stormstrike', 'player.buff(Stormbringer)'},
+	{'Frostbrand', 'talent(4,3)&player.buff(Frostbrand).remains<gcd'},
+	{'Flametongue', 'player.buff(Flametongue).remains<gcd'},
+	{'Windsong'},
+	{'Fury of Air', 'talent(6,2)&!player.buff(Fury of Air)'},
+	{'Stormstrike'},
+	{'Lightning Bolt', 'talent(5,2)&player.maelstrom>50'},
+	{'Lava Lash', 'player.buff(Hot Hand)'},
+	{'Earthen Spike'},
+	{'Frostbrand', 'talent(4,3)&player.buff(Frostbrand).remains<4.5'},
+	{'Flametongue', 'player.buff(Flametongue).remains<4.5'},
+	{'Sundering'},
+	{'Rockbiter'},
+	{'Flametongue'}
 }
 
 local Ranged = {
@@ -144,11 +138,11 @@ local inCombat = {
 	{Heirlooms},
 	{Keybinds},
 	{Interrupts_Random},
-	{Interrupts, 'target.interruptAt(70)&toggle(Interrupts)&target.inFront&target.range<41'},
+	{Interrupts, 'target.interruptAt(70)&toggle(Interrupts)&target.range<41'},
 	{Survival},
 	{Party},
 	{Cooldowns, 'toggle(Cooldowns)'},
-	{xCombat, 'target.range<=8&target.inFront'},
+	{xCombat, 'target.inMelee&target.inFront'},
 	{Ranged}
 }
 
@@ -156,12 +150,11 @@ local outCombat = {
 	{Keybinds},
 	{PreCombat},
 	{Interrupts_Random},
-	{Interrupts, 'target.interruptAt(70)&toggle(Interrupts)&target.inFront&target.range<40'},
-	{'!Healing Surge', '!moving&UI(E_HS)&player.health<90', 'player'},
+	{Interrupts, 'target.interruptAt(70)&toggle(Interrupts)&target.inFront&target.range<41'}
 }
 
 NeP.CR:Add(263, {
-	name = '[|cff'..Zylla.addonColor..'Zylla\'s|r] Shaman - Enhancement (Default)',
+	name = '[|cff'..Zylla.addonColor..'Zylla\'s|r] Shaman - Enhancement (default)',
 	ic = inCombat,
 	ooc = outCombat,
 	gui = GUI,
