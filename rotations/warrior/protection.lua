486c554d05250d1b4d601793784fcab5227e4927
Andreas <adde88@gmail.com>
2017-08-17 21:51:57 +0200

    Prot-update.

--- rotations/warrior/protection.lua
+++ rotations/warrior/protection.lua
@@ -5,6 +5,9 @@ local Trinkets = _G['Zylla.Trinkets']
 local Heirlooms = _G['Zylla.Heirlooms']
 
 local GUI = {
+	--Logo
+	{type = "texture", texture = "Interface\\AddOns\\Nerdpack-Zylla\\media\\logo.blp", width = 128, height = 128, offset = 90, y = 42, center = true},
+	{type = 'ruler'},	 {type = 'spacer'},
 	--KEYBINDS
 	{type = 'header', 	text = 'Keybinds', align = 'center'},
 	{type = 'text', 	text = 'Left Shift: Pause', align = 'center'},
@@ -17,11 +20,12 @@ local GUI = {
 	{type = 'ruler'},	{type = 'spacer'},
 	-- Trinkets + Heirlooms for leveling
 	{type = 'header', 	text = 'Trinkets/Heirlooms', align = 'center'},
-	{type = 'checkbox', text = 'Use Trinket #1', key = 'kT1', default = true},
-	{type = 'checkbox', text = 'Use Trinket #2', key = 'kT2', default = true},
-	{type = 'checkbox', text = 'Ring of Collapsing Futures', key = 'kRoCF', default = true},
-	{type = 'checkbox', text = 'Use Heirloom Necks When Below X% HP', key = 'k_HEIR', default = true},
+	{type = 'checkbox', text = 'Use Trinket #1', key = 'kT1', default = false},
+	{type = 'checkbox', text = 'Use Trinket #2', key = 'kT2', default = false},
+	{type = 'checkbox', text = 'Ring of Collapsing Futures', key = 'kRoCF', default = false},
+	{type = 'checkbox', text = 'Use Heirloom Necks When Below X% HP', key = 'k_HEIR', default = false},
 	{type = 'spinner',	text = '', key = 'k_HeirHP', default = 40},
+	{type = 'ruler'},	{type = 'spacer'},
 }
 
 local exeOnLoad = function()
@@ -31,16 +35,26 @@ local exeOnLoad = function()
 	print("|cffADFF2F ----------------------------------------------------------------------|r")
 	print("|cffADFF2F --- |rWarrior |cffADFF2FProtection |r")
 	print("|cffADFF2F --- |rRecommended Talents: 1/1 - 2/1 - 3/2 - 4/2 - 5/3 - 6/1 - 7/2")
-	print("|cffADFF2F ----------------------------------------------------------------------|r")
+	print('|cffADFF2F ----------------------------------------------------------------------|r')
+	print('|cffFFFB2F Configuration: |rRight-click MasterToggle and go to Combat Routines Settings!|r')
+
+	NeP.Interface:AddToggle({
+		key = 'xIntRandom',
+		name = 'Interrupt Anyone',
+		text = 'Interrupt all nearby enemies, without targeting them.',
+		icon = 'Interface\\Icons\\inv_ammo_arrow_04',
+	})
+
+	NeP.Interface:AddToggle({
+ 	key = 'super_taunt',
+ 	name = 'Taunt Lowest Threat',
+ 	text = 'Taunt a nearby enemy in combat, when threat gets low, without targeting it.',
+ 	icon = 'Interface\\Icons\\spell_nature_reincarnation',
+ })
 
 end
 
-local Etc = {
-	{'Impending Victory', '{!player.buff(Victorious)&player.rage>10&player.health<95}||{player.buff(Victorious)&player.health<80}'},
-	{'Heroic Throw', '!target.inMelee&target.range<40&target.inFront'},
-	{'Shockwave', '!target.immune(stun)&player.area(6).enemies>2&target.inMelee&target.inFront'},
-	{'Intercept', '!target.inMelee&target.range<25&target.enemy&!prev_gcd(Heroic Leap)&UI(kIntercept)&target.alive'},
-}
+local PreCombat = {}
 
 local Keybinds = {
 	-- Pause
@@ -48,50 +62,52 @@ local Keybinds = {
 	{'!Heroic Leap', 'keybind(lcontrol)' , 'cursor.ground'}
 }
 
+local Etc = {
+	{'Impending Victory', 'inMelee&{{!player.buff(Victorious)&player.rage>10&player.health<95}||{player.buff(Victorious)&player.health<80}}', 'target'},
+	{'Heroic Throw', 'range>10&range<40&inFront', 'target'},
+	{'Shockwave', '!target.immune(stun)&player.area(6).enemies>2&target.inMelee&target.inFront'},
+	{'Intercept', 'range>10&range<25&enemy&alive&combat&!player.lastgcd(Heroic Leap)&UI(kIntercept)', 'target'},
+	{'Heroic Throw', 'range>10&range<41&inFront&alive&enemy&!UI(kIntercept', 'target'}
+}
+
 local Interrupts = {
-	{'!Pummel', 'target.inFront&target.inMelee'},
-	{'!Arcane Torrent', 'target.inMelee&spell(Pummel).cooldown>gcd&!prev_gcd(Pummel)'},
-	{'!Shockwave', 'talent(1,1)&!target.immune(stun)&spell(Pummel).cooldown>gcd&!prev_gcd(Pummel)&target.inFront&target.inMelee'},
-	{'!Spell Reflection', '{target.inMelee&spell(Pummel).cooldown>gcd&!prev_gcd(Pummel)}||{target.range>00&!spell(Pummel).cooldown}'}
+	{'!Pummel', 'inFront&inMelee', 'target'},
+	{'!Arcane Torrent', 'inMelee&player.spell(Pummel).cooldown>gcd&!player.lastgcd(Pummel)', 'target'},
+	{'!Shockwave', 'talent(1,1)&!target.immune(stun)&spell(Pummel).cooldown>gcd&!player.lastgcd(Pummel)&target.inFront&target.inMelee'},
+	{'!Spell Reflection', 'target.inMelee&player.spell(Shockwave).cooldown>gcd&player.spell(Pummel).cooldown>gcd'}
 }
 
-local Cooldowns = {}
-
-local PreCombat = {
-	{'Intercept', '!target.inMelee&target.range<25&target.enemy&!prev_gcd(Heroic Leap)&UI(kIntercept)&target.alive'},
-	{'Heroic Throw', '!target.inMelee&target.range<40&target.inFront&target.alive&target.enemy&!UI(kIntercept'}
+local Interrupts_Random = {
+	{'!Pummel', 'interruptAt(70)&toggle(xIntRandom)&toggle(Interrupts)&inFront&inMelee', 'enemies'},
+	{'!Shockwave', 'interruptAt(70)&&!target.immune(stun)&toggle(xIntRandom)&toggle(Interrupts)&player.spell(Pummel).cooldown>gcd&!player.lastgcd(Pummel)&inFront&inMelee', 'enemies'},
+	{'!Spell Reflection', 'interruptAt(70)&toggle(xIntRandom)&toggle(Interrupts)&player.spell(Shockwave).cooldown>gcd&player.spell(Pummel).cooldown>gcd&range<51', 'enemies.ground'},
 }
 
-local Something = {
-	{'Focused Rage', 'talent(3,2)&player.buff(Ultimatum)&!talent(6,1)'},
-	{'Battle Cry', '{talent(6,1)&talent(3,2)&spell(Shield Slam).cooldown<5.5-gcd}||!talent(6,1)'},
+local Cooldowns = {
 	{'Demoralizing Shout', 'talent(6,3)&player.buff(Battle Cry)'},
-	{'Ravager', 'talent(7,3)&player.buff(Battle Cry)'}
+	{'Shield Wall', 'player.incdmg(2.5)>player.health.max*0.50'},
+	{'Last Stand', 'player.incdmg(2.5)>player.health.max*0.50&!player.spell(Shield Wall).cooldown'},
+	{'Battle Cry', '{talent(6,1)&talent(3,2)&player.spell(Shield Slam).cooldown<5.5-gcd}||!talent(6,1)'},
+	{'Ravager', 'talent(7,3)&player.buff(Battle Cry)', 'target.ground'},
+	{'#144249', 'equipped(144249)&{player.area(8).enemies>4||player.incdmg(2.5)>player.health.max*0.20}'}, -- Archimonde's Hatred Reborn (Trinket)
 }
 
 local AoE = {
-	{Something},
-	{'Neltharion\'s Fury', 'player.buff(Battle Cry)&xmoving==0&player.area(8).enemies>2'},
-	{'Shield Slam', '!{spell(Shield Block).cooldown<=gcd*2&!player.buff(Shield Block)&talent(7,2)}'},
-	{'Revenge'},
+	{'Neltharion\'s Fury', '{player.buff(Battle Cry)&!player.moving&player.area(8).enemies>2}||player.incdmg(2.5)>player.health.max*0.20'},
+	{'Shield Slam', 'player.spell(Shield Block).cooldown<gcd*2&!player.buff(Shield Block)&talent(7,2)', 'target'},
+	{'Revenge', nil, 'target'},
 	{'Thunder Clap', 'player.area(6).enemies>2'},
-	{'Devastate'}
+	{'Devastate', '!talent(5,1)', 'target'}
 }
 
 local ST = {
 	{'Shield Block', '!player.buff(Neltharion\'s Fury)&{{spell(Shield Slam).cooldown<6&!player.buff(Shield Block)}||{spell(Shield Slam).cooldown<6+player.buff(Shield Block).duration&player.buff(Shield Block)}}'},
-	{'!Ignore Pain','{player.buff(Vengeance: Ignore Pain)&player.buff(Ignore Pain).duration<=gcd*2&player.rage>30}||{!player.buff(Vengeance: Ignore Pain)&player.buff(Ignore Pain).duration<=gcd*2&player.rage>10}||{player.rage>50&!talent(6,1)}||{player.buff(Vengeance: Ignore Pain)&player.buff(Ultimatum)}||{player.buff(Vengeance: Ignore Pain)&player.rage>20}||{talent(6,1)&!player.buff(Ultimatum)&!player.buff(Vengeance: Ignore Pain)&!player.buff(Vengeance: Focused Rage)&player.rage<30}'},
-	{'Focused Rage', '{player.buff(Vengeance: Focused Rage)&!player.buff(Vengeance: Ignore Pain)}||{player.buff(Ultimatum)&player.buff(Vengeance: Focused Rage)&!player.buff(Vengeance: Ignore Pain)}||{talent(6,1)&player.buff(Ultimatum)&!player.buff(Vengeance: Ignore Pain)&!player.buff(Vengeance: Focused Rage)}||{talent(6,1)&!player.buff(Vengeance: Ignore Pain)&!player.buff(Vengeance: Focused Rage)&player.rage>20}||{player.buff(Ultimatum)&player.buff(Vengeance: Ignore Pain)&spell(Shield Slam)&player.rage<10}||{player.rage>000}'},
-	{'Demoralizing Shout', 'player.incdmg(2.5)>player.health.max*0.20'},
-	{'Shield Wall', 'player.incdmg(2.5)>player.health.max*0.50'},
-	{'Last Stand', 'player.incdmg(2.5)>player.health.max*0.50&!spell(Shield Wall).cooldown'},
-	{AoE, 'toggle(aoe)&player.area(8).enemies>1'},
-	{Something},
-	{'Neltharion\'s Fury', 'player.incdmg(2.5)>player.health.max*0.20&!player.buff(Shield Block)'},
-	{'Shield Slam', '!{spell(Shield Block).cooldown<=gcd&!player.buff(Shield Block)&talent(7,2)}||{player.buff(Vengeance: Ignore Pain)&player.buff(Ignore Pain).duration<=gcd*2&player.rage<13}||{!player.buff(Vengeance: Ignore Pain)&player.buff(Ignore Pain).duration<=gcd*2&player.rage<20}||'},
-	{'Shield Slam', '!talent(7,2)'},
-	{'Revenge', 'spell(Shield Slam).cooldown<=gcd*2||player.rage<6'},
-	{'Devastate'}
+	{'!Ignore Pain','{player.buff(Vengeance: Ignore Pain)&player.buff(Ignore Pain).duration<=gcd*2&player.rage>30}||{!player.buff(Vengeance: Ignore Pain)&player.buff(Ignore Pain).duration<=gcd*2&player.rage>10}||{player.rage>50&!talent(6,1)}||{player.buff(Vengeance: Ignore Pain)&player.buff(Ultimatum)}||{player.buff(Vengeance: Ignore Pain)&player.rage>20}||{talent(6,1)&!player.buff(Ultimatum)&!player.buff(Vengeance: Ignore Pain)&!player.buff(Vengeance: Focused Rage)&player.rage<30}', 'target'},
+	{'Neltharion\'s Fury', 'player.incdmg(2.5)>player.health.max*0.20&!player.buff(Shield Block)&!player.moving'},
+	{'Shield Slam', '{player.spell(Shield Block).cooldown<gcd&!player.buff(Shield Block)&talent(7,2)}||{player.buff(Vengeance: Ignore Pain)&player.buff(Ignore Pain).duration<gcd*2&player.rage<13}||{!player.buff(Vengeance: Ignore Pain)&player.buff(Ignore Pain).duration<gcd*2&player.rage<20}', 'target'},
+	{'Shield Slam', '!talent(7,2)', 'target'},
+	{'Revenge', 'player.spell(Shield Slam).cooldown<gcd*2||player.rage<6', 'target'},
+	{'Devastate', '!talent(5,1)', 'target'}
 }
 
 local inCombat = {
@@ -100,14 +116,18 @@ local inCombat = {
 	{Heirlooms},
 	{Etc},
 	{Keybinds},
-	{Interrupts, 'target.interruptAt(70)&toggle(Interrupts)'},
-	{Cooldowns, 'toggle(Cooldowns)'},
-	{ST, 'target.inMelee&target.inFront'},
+	{Interrupts_Random},
+	{Interrupts, 'target.interruptAt(70)&toggle(interrupts)'},
+	{Cooldowns, 'toggle(cooldowns)'},
+	{ST, 'target.inMelee&target.inFront&player.area(8).enemies<2'},
+	{AoE, 'toggle(aoe)&target.inMelee&target.inFront&player.area(8).enemies>1'},
+	{'Taunt', 'toggle(super_taunt)&combat&alive&threat<100', 'enemies'},
 }
 
 local outCombat = {
 	{Keybinds},
 	{PreCombat},
+	{Interrupts_Random},
 	{Interrupts, 'target.interruptAt(70)&toggle(Interrupts)'},
 }
 
