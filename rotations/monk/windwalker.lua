6fe06c7165cc11e59dadfe678068a988c7e37f6c
Andreas <adde88@gmail.com>
2017-08-28 10:23:23 +0200

    Moved Auto-face toggle to settings + Added Paralysis as interrupts (derp!) + fixed an issue with random interrupts + general cleanup

--- rotations/monk/windwalker.lua
+++ rotations/monk/windwalker.lua
@@ -12,6 +12,7 @@ local GUI = {
 	{type = 'ruler'},	 {type = 'spacer'},
 	-- General
 	{type = 'header',		text = 'General:',																				align = 'center'},
+	{type = 'checkbox', text = 'Auto-target when casting Fists of Fury', 					key = 'xfistface', 	default = true},
 	{type = 'checkbox',	text = 'Automatic Res',																		key = 'auto_res',		default = true},
 	{type = 'checkbox',	text = '5 min DPS test', 																	key = 'dpstest',		default = false},
 	{type = 'checkbox',	text = 'Pause Enabled', 																	key = 'kPause',			default = true},
@@ -30,6 +31,7 @@ local GUI = {
 	{type = 'spacer'},
 	{type = 'checkspin',	text = 'Effuse Party-Member HP%',												key = 'effp',					spin = 60, check = false},
 	{type = 'checkbox',		text = 'Dispel Party Members', 													key = 'E_disAll',			default = true},
+	{type = 'checkbox', 	text = 'Use Paralysis to Interrupt', 										key = 'para', 				default = true},
 	-- Offensive
 	{type = 'spacer'},	{type = 'rule'},
 	{type = 'header',		text = 'Offensive:',																			align = 'center'},
@@ -66,13 +68,6 @@ local exeOnLoad = function()
 		icon = 'Interface\\Icons\\inv_ammo_arrow_04',
 	})
 
-	NeP.Interface:AddToggle({
-		key = 'xFistFace',
-		name = 'Auto-Face',
-		text = 'Automatically Face your target while casting Fists of Fury',
-		icon = 'Interface\\Icons\\monk_ability_fistoffury',
-	})
-
 end
 
 local Keybinds = {
@@ -82,10 +77,9 @@ local Keybinds = {
 	{'!Transcendence', 'keybind(lcontrol)&!player.buff(Transcendence)'},
 	{'!Transcendence: Transfer', 'keybind(lcontrol)&player.buff(Transcendence)'},
 	{'!/cancelaura Transcendence', 'keybind(lcontrol)&player.buff(Transcendence)&lastcast(Transcendence: Transfer)'},
-	-- Etc...
-	{'!Tiger\'s Lust', 'player.state(disorient)||player.state(stun)||player.state(root)||player.state(snare)'},		-- FREEDOOM!
-	{'&/stopcasting\n/stopattack\n/cleartarget\n/stopattack\n/cleartarget\n/nep mt', 'player.combat.time>200&UI(dpstest)'},		-- DPS TEST
-	{'&/stopcasting', 'target.range<=5&player.casting(Crackling Jade Lightning)'},		-- Cancel CJL WHEN WE'RE IN MELEE RANGE
+	{'!Tiger\'s Lust', 'player.state(disorient)||player.state(stun)||player.state(root)||player.state(snare)'},
+	{'&/stopcasting\n/stopattack\n/cleartarget\n/stopattack\n/cleartarget\n/nep mt', 'player.combat.time>200&UI(dpstest)'},
+	{'&/stopcasting', 'target.range<=5&player.casting(Crackling Jade Lightning)'}
 }
 
 local Cooldowns = {
@@ -100,12 +94,12 @@ local Cooldowns = {
 	{'#trinket2', 'UI(trinket2)&{player.buff(Serenity)||player.buff(Storm, Earth, and Fire)}'},
 	-- Use Xuen only while hero or potion (WOD: 156423, Legion: 188027) is active
 	{'Invoke Xuen, the White Tiger', 'player.hashero||{player.buff(Serenity)||player.buff(Storm, Earth, and Fire)}'},
-	{'Touch of Karma', 'UI(tok_check){player.health<=UI(tok_spin)||player.incdmg(5)>player.health.max*0.20}'},
+	{'Touch of Karma', 'UI(tok_check){player.health<=UI(tok_spin)||player.incdmg(5)>player.health.max*0.20}'}
 }
 
 local Dispel = {
 	{'%dispelSelf'},
-	{'%dispelAll', 'UI(E_disAll)'},
+	{'%dispelAll', 'UI(E_disAll)'}
 }
 
 local Survival = {
@@ -113,21 +107,23 @@ local Survival = {
 	{'#127834', 'item(127834).usable&item(127834).count>0&player.health<=UI(ahp_spin)&UI(ahp_check)'}, 		-- Ancient Healing Potion
 	{'#5512', 'item(5512).usable&item(5512).count>0&player.health<=UI(hs_spin)&UI(hs_check)', 'player'}, 	-- Health Stone
 	{'!Effuse', 'energy>50&!moving&UI(eff_check)&health<=UI(eff_spin)', 'player'}, 												-- Self  healing. Toggle and HP% in Settings
-	{'!Effuse', 'energy>50&!player.moving&UI(effp_check)&health<=UI(effp_spin)', 'lowest'}, 							-- Party healing. Toggle and HP% in Settings
+	{'!Effuse', 'energy>50&!player.moving&UI(effp_check)&health<=UI(effp_spin)', 'lowest'} 								-- Party healing. Toggle and HP% in Settings
 }
 
 local Interrupts = {
 	{'!Spear Hand Strike', 'interruptAt(70)&range<=5&inFront', 'target'},
+	{'!Paralysis', 'UI(para)&interruptAt(60)&!immune(incapacitate)&range<21&inFront&player.spell(Spear Hand Strike).cooldown>gcd&!player.lastcast(Spear Hand Strike)', 'target'},
 	{'!Ring of Peace', 'interruptAt(5)&advanced&range<40&player.spell(Spear Hand Strike).cooldown>gcd&!player.lastgcd(Spear Hand Strike)', 'target.ground'},
 	{'!Leg Sweep', 'interruptAt(70)&!immune(stun)&range<=5&player.spell(Spear Hand Strike).cooldown>gcd&!player.lastcast(Spear Hand Strike)', 'target'},
-	{'!Quaking Palm', 'interruptAt(70)&!immune(incapacitate)&range<=5&inFront&player.spell(Spear Hand Strike).cooldown>gcd&!player.lastcast(Spear Hand Strike)', 'target'},
+	{'!Quaking Palm', 'interruptAt(70)&!immune(incapacitate)&range<=5&inFront&player.spell(Spear Hand Strike).cooldown>gcd&!player.lastcast(Spear Hand Strike)', 'target'}
 }
 
 local Interrupts_Random = {
-	{'!Rebuke', 'interruptAt(70)&toggle(xIntRandom)&toggle(Interrupts)&inFront&range<=5', 'enemies'},
+	{'!Spear Hand Strike', 'interruptAt(70)&toggle(xIntRandom)&toggle(Interrupts)&inFront&range<=5', 'enemies'},
+	{'!Paralysis', 'UI(para)&interruptAt(60)&!immune(incapacitate)&range<21&inFront&player.spell(Spear Hand Strike).cooldown>gcd&!player.lastcast(Spear Hand Strike)', 'enemies'},
 	{'!Ring of Peace', 'interruptAt(5)&advanced&range<40&player.spell(Spear Hand Strike).cooldown>gcd&!player.lastcast(Spear Hand Strike)', 'enemies.ground'},
 	{'!Leg Sweep', 'interruptAt(70)&!immune(stun)&range<=5&player.spell(Spear Hand Strike).cooldown>gcd&!player.lastgcd(Spear Hand Strike)', 'enemies'},
-	{'!Quaking Palm', 'interruptAt(70)&!immune(incapacitate)&range<=5&inFront&player.spell(Spear Hand Strike).cooldown>gcd&!player.lastcast(Spear Hand Strike)', 'enemies'},
+	{'!Quaking Palm', 'interruptAt(70)&!immune(incapacitate)&range<=5&inFront&player.spell(Spear Hand Strike).cooldown>gcd&!player.lastcast(Spear Hand Strike)', 'enemies'}
 }
 
 local SEF = {
@@ -142,11 +138,11 @@ local SEF = {
 local Ranged = {
 	{'Tiger\'s Lust', 'player.movingfor>0.5&target.combat&target.alive'},
 	{'Chi Wave', 'toggle(aoe)&UI(auto_cw)&player.area(40).enemies>=2', 'target'},
-	{'Chi Wave', 'toggle(aoe)&player.area(40).enemies>=2&player.timetomax>1.25', 'target'}, -- 40 yard range 0 energy, 0 chi
+	{'Chi Wave', 'toggle(aoe)&player.area(40).enemies>=2&player.timetomax>1.25', 'target'},
 	{'Chi Burst', 'toggle(aoe)&player.area(40).enemies.infront>=2&target.range<=40&!player.moving&player.timetomax>1.25', 'target'},
 	{'Crackling Jade Lightning', '!player.moving&UI(auto_cjl)&player.combat.time>4&!player.lastgcd(Crackling Jade Lightning)&@Zylla.hitcombo(Crackling Jade Lightning)', 'target'},
-	{'Crackling Jade Lightning', '!player.moving&equipped(144239)&player.buff(The Emperor\'s Capacitor).count>10&!player.lastgcd(Crackling Jade Lightning)&@Zylla.hitcombo(Crackling Jade Lightning)&player.energydiff==0', 'target'}, -- Legendary Chest support
-	{'Crackling Jade Lightning', '!player.moving&UI(auto_cjl_hc)&!player.lastgcd(Crackling Jade Lightning)&@Zylla.hitcombo(Crackling Jade Lightning)&player.energydiff==0', 'target'}, -- CJL when we're using Hit Combo as a last resort filler, at 100 energy, and it's toggled on
+	{'Crackling Jade Lightning', '!player.moving&equipped(144239)&player.buff(The Emperor\'s Capacitor).count>10&!player.lastgcd(Crackling Jade Lightning)&@Zylla.hitcombo(Crackling Jade Lightning)&player.energydiff==0', 'target'},
+	{'Crackling Jade Lightning', '!player.moving&UI(auto_cjl_hc)&!player.lastgcd(Crackling Jade Lightning)&@Zylla.hitcombo(Crackling Jade Lightning)&player.energydiff==0', 'target'}
 }
 
 local Serenity = {
@@ -160,7 +156,7 @@ local Serenity = {
 	{'Fists of Fury', 'inFront', 'target'},
 	{'Blackout Kick', 'UI(auto_dot)&!player.lastgcd(Blackout Kick)&@Zylla.hitcombo(Blackout Kick)', 'Zylla_sck(Mark of the Crane)'},
 	{'Blackout Kick', '!player.lastgcd(Blackout Kick)&@Zylla.hitcombo(Blackout Kick)&target.range<=5', 'target'},
-	{'Rushing Jade Wind', '!player.lastgcd(Rushing Jade Wind)&@Zylla.hitcombo(Rushing Jade Wind)'},
+	{'Rushing Jade Wind', '!player.lastgcd(Rushing Jade Wind)&@Zylla.hitcombo(Rushing Jade Wind)'}
 }
 
 local Melee = {
@@ -187,7 +183,7 @@ local Melee = {
 	{'Blackout Kick', 'player.chi==1&!player.buff(Hit Combo)'},	-- Last resort BoK when we only have 1 chi and no hit combo
 	{'Tiger Palm', 'player.combat.time<4&player.energydiff==0&player.chi<2&!player.lastgcd(Tiger Palm)&@Zylla.hitcombo(Tiger Palm)&target.range<=5'},
 	{'Tiger Palm', '!player.buff(Hit Combo)', 'target'},	-- Last resort TP when we don't have hit combo up
-	{'Tiger Palm', 'player.energy>100', 'target'},
+	{'Tiger Palm', 'player.energy>100', 'target'}
 }
 
 local inCombat = {
@@ -199,7 +195,7 @@ local inCombat = {
 	{Serenity, 'toggle(cooldowns)&target.range<=5'},
 	{SEF, 'target.range<=5&UI(sef_toggle)&!talent(7,3)&player.spell(Strike of the Windlord).cooldown<24&player.spell(Fists of Fury).cooldown<7&player.spell(Rising Sun Kick).cooldown<7'},
 	{Melee, 'target.range<=5&target.inFront'},
-	{Ranged, '!target.range<=5&target.range<41'},
+	{Ranged, 'target.range>5&target.range<41'}
 }
 
 local outCombat = {
@@ -215,7 +211,7 @@ NeP.CR:Add(269, {
 	name='[|cff'..Zylla.addonColor..'Zylla\'s|r] Monk - Windwalker',
 	ic = {
 		{inCombat, '!player.casting(Fists of Fury)'},
-		{'&@Zylla.face', 'toggle(xFistFace)&player.casting(Fists of Fury)', 'target'}
+		{'&@Zylla.face', 'UI(xfistface)&player.casting(Fists of Fury)', 'target'}
 	},
 	ooc = outCombat,
 	gui = GUI,
