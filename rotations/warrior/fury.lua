3b7e01c1eaac87fde88cfac97c0dbafac4874edc
Andreas Nilsen <adde88@gmail.com>
2017-09-06 22:59:51 +0200

    Lets cleanup the CR a little bit. More perhaps? Testing needed.

--- rotations/warrior/fury.lua
+++ rotations/warrior/fury.lua
@@ -21,6 +21,7 @@ local GUI = {
 	{type = 'ruler'},	{type = 'spacer'},
 	-- Survival
 	{type = 'header', 		size = 16, text = 'Survival',							align = 'center'},
+	{type = 'checkspin',	text = 'Victory Rush below HP%',					key = 'vrush',				spin = 80, check = true},
 	{type = 'checkspin',	text = 'Healthstone',											key = 'HS',						spin = 45, check = true},
 	{type = 'checkspin',	text = 'Healing Potion',									key = 'AHP',					spin = 45, check = true},
 	unpack(Mythic_GUI),
@@ -36,6 +37,13 @@ local exeOnLoad = function()
 	print('|cffADFF2F --- |rLast Updated: 16.06.2017')
 	print('|cffADFF2F ----------------------------------------------------------------------|r')
 
+	NeP.Interface:AddToggle({
+		key = 'xIntRandom',
+		name = 'Interrupt Anyone',
+		text = 'Interrupt all nearby enemies, without targeting them.',
+		icon = 'Interface\\Icons\\inv_ammo_arrow_04',
+	})
+
 end
 
 local Keybinds = {
@@ -45,89 +53,96 @@ local Keybinds = {
 }
 
 local Interrupts = {
-	{'!Pummel', 'target.inMelee&target.inFront'},
-	{'!Storm Bolt', 'target.inFront&target.range<30&talent(2,2)&!target.immune(stun)&spell(Pummel).cooldown>gcd&!prev_gcd(Pummel)'},
-	{'!Arcane Torrent', 'target.inMelee&spell(Pummel).cooldown>gcd&!prev_gcd(Pummel)'},
-	{'!Shockwave', 'target.inFront&target.inMelee&talent(2,1)&!target.immune(stun)&spell(Pummel).cooldown>gcd&!prev_gcd(Pummel)'},
+	{'!Pummel', 'range<=5&inFront', 'target'},
+	{'!Storm Bolt', 'inFront&range<30&talent(2,2)&!immune(stun)&spell(Pummel).cooldown>gcd&!prev_gcd(Pummel)', 'target'},
+	{'!Arcane Torrent', 'range<=5&spell(Pummel).cooldown>gcd&!prev_gcd(Pummel)', 'target'},
+	{'!Shockwave', 'inFront&range<=5&talent(2,1)&!immune(stun)&spell(Pummel).cooldown>gcd&!prev_gcd(Pummel)', 'target'},
+}
+
+local Interrupts_Random = {
+	{'!Pummel', 'interruptAt(70)&toggle(xIntRandom)&toggle(Interrupts)&inFront&range<=5', 'enemies'},
+	{'!Storm Bolt', 'interruptAt(70)&toggle(xIntRandom)&toggle(Interrupts)&player.spell(Pummel).cooldown>gcd&!prev_gcd(Pummel)&inFront&range<21', 'enemies'},
+	{'!Arcane Torrent', 'interruptAt(70)&toggle(xIntRandom)&toggle(Interrupts)&player.spell(Pummel).cooldown>gcd&!prev_gcd(Pummel)&inFront&range<=5', 'enemies'},
+	{'!Shockwave', 'interruptAt(75)&toggle(xIntRandom)&toggle(Interrupts)&player.spell(Pummel).cooldown>gcd&!prev_gcd(Pummel)&inFront&range<11', 'enemies'},
 }
 
 local Survival = {
-	{'Victory Rush', 'player.health<80'},
-	{'#127834', 'item(127834).usable&item(127834).count>0&player.health<=UI(AHP_spin)&UI(AHP_check)'}, 		-- Ancient Healing Potion
-	{'#5512', 'item(5512).usable&item(5512).count>0&player.health<=UI(HS_spin)&UI(HS_check)', 'player'}, 	--Health Stone
+	{'Victory Rush', 'player.health<=UI(vrush_spin)&UI(vrush_check)', 'target'},
+	{'#127834', 'item(127834).usable&item(127834).count>0&health<=UI(AHP_spin)&UI(AHP_check)', 'player'}, 		-- Ancient Healing Potion
+	{'#5512', 'item(5512).usable&item(5512).count>0&player.health<=UI(HS_spin)&UI(HS_check)', 'player'}, 			--Health Stone
 }
 
 local Cooldowns = {
 	{'Battle Cry', '{spell(Odyn\'s Fury).cooldown<gcd&{spell(Bloodthirst).cooldown<gcd||{player.buff(Enrage).remains>spell(Bloodthirst).cooldown}}}'},
-	{'Avatar', 'talent(3,3)&player.buff(Battle Cry)'},
-	{'Bloodbath', 'talent(6,1)&{player.buff(Dragon Roar)||{!talent(7,3)&{player.buff(Battle Cry)||spell(Battle Cry).cooldown>10}}}'},
-	{'Blood Fury', 'player.buff(Battle Cry)'},
-	{'Berserking', 'player.buff(Battle Cry)'},
+	{'Avatar', 'buff(Battle Cry)', 'player'},
+	{'Bloodbath', 'player.buff(Dragon Roar)||{!talent(7,3)&{player.buff(Battle Cry)||spell(Battle Cry).cooldown>10}}', 'target'},
+	{'Blood Fury', 'buff(Battle Cry)', 'player'},
+	{'Berserking', 'buff(Battle Cry)', 'player'},
 	{'#Trinket1', 'UI(trinket1)'},
-	{'#Trinket2', 'UI(trinket2)'},
+	{'#Trinket2', 'UI(trinket2)'}
 }
 
 local Bladestorm = {
-	{'Bladestorm', 'talent(7,1)&player.buff(Enrage).remains>2'},
+	{'Bladestorm', 'player.buff(Enrage).remains>2', 'target'}
 }
 
 local Ranged = {
-	{'Storm Bolt', 'talent(2,2)&target.range<30'},
+	{'Storm Bolt', 'range<30', 'target'},
 }
 
 local AoE = {
-	{'Bloodthirst', '!player.buff(Enrage)||player.rage<50'},
+	{'Bloodthirst', '!player.buff(Enrage)||player.rage<50', 'target'},
+	{'Odyn\'s Fury', 'player.buff(Battle Cry)&player.buff(Enrage)', 'target'},
 	{Bladestorm},
-	{'Whirlwind', 'player.buff(Enrage)'},
-	{'Dragon Roar', 'talent(7,3)'},
-	{'Rampage', 'player.buff(Meat Cleaver)'},
-	{'Bloodthirst'},
-	{'Whirlwind'},
-	{'Shockwave', 'talent(2,1)&!target.immune(stun)&player.area(6).enemies>2&target.inMelee&target.inFront'},
+	{'Whirlwind', 'player.buff(Enrage)', 'target'},
+	{'Dragon Roar', 'talent(7,3)', 'target'},
+	{'Rampage', 'buff(Meat Cleaver)', 'player'},
+	{'Bloodthirst', nil, 'target'},
+	{'Whirlwind', nil, 'target'},
+	{'Shockwave', '!immune(stun)&player.area(10).enemies.inFront>2', 'target'},
 }
 
 local ST = {
-	{'Bloodthirst', 'player.buff(Fujieda\'s Fury)&player.buff(Fujieda\'s Fury).stack<2'},
-	{'Execute', '{artifact(Juggernaut).enabled&{!player.buff(Juggernaut)||player.buff(Juggernaut).remains<2}}||player.buff(Stone Heart)'},
-	{'Rampage', 'player.rage==100&{target.health>20||{target.health<20&!talent(5,1)}||{player.buff(Massacre)&player.buff(Enrage).remains<1}}'},
-	{'Berserker Rage', 'talent(3,2)&spell(Odyn\'s Fury).cooldown<gcd&!player.buff(Enrage)'},
-	{'Dragon Roar', '!spell(Odyn\'s Fury).cooldown<20||spell(Odyn\'s Fury).cooldown<3'},
-	{'Odyn\'s Fury', 'artifact(Odyn\'s Fury).equipped&player.buff(Battle Cry)&player.buff(Enrage)'},
-	{'Rampage', '!player.buff(Enrage)&!player.buff(Juggernaut)'},
-	{'Furious Slash', 'talent(6,2)&{!player.buff(Frenzy)||player.buff(Frenzy).stack<4}'},
-	{'Raging Blow', '!player.buff(Juggernaut)&player.buff(Enrage)'},
-	{'Whirlwind', 'talent(3,1)&player.buff(Wrecking Ball)&player.buff(Enrage)'},
-	{'Execute', 'talent(6,3)||{!talent(6,3)&player.rage>50}'},
-	{'Bloodthirst', '!player.buff(Enrage)'},
-	{'Raging Blow', '!player.buff(Enrage)'},
-	{'Execute', 'artifact(Juggernaut).enabled'},
-	{'Raging Blow'},
-	{'Bloodthirst'},
-	{'Furious Slash'},
+	{'Bloodthirst', 'player.buff(Fujieda\'s Fury).stack<2', 'target'},
+	{'Execute', '{!player.buff(Juggernaut)||player.buff(Juggernaut).remains<2}}||player.buff(Stone Heart)', 'target'},
+	{'Rampage', 'player.rage==100&{target.health>20||{target.health<20&!talent(5,1)}||{player.buff(Massacre)&player.buff(Enrage).remains<1}}', 'player'},
+	{'Berserker Rage', 'talent(3,2)&spell(Odyn\'s Fury).cooldown<gcd&!buff(Enrage)', 'player'},
+	{'Dragon Roar', 'player.spell(Odyn\'s Fury).cooldown>20||player.spell(Odyn\'s Fury).cooldown<3', 'target'},
+	{'Odyn\'s Fury', 'player.buff(Battle Cry)&player.buff(Enrage)', 'target'},
+	{'Rampage', '!player.buff(Enrage)&!player.buff(Juggernaut)', 'target'},
+	{'Furious Slash', 'talent(6,2)&{!player.buff(Frenzy)||player.buff(Frenzy).stack<4}', 'target'},
+	{'Raging Blow', '!player.buff(Juggernaut)&player.buff(Enrage)', 'target'},
+	{'Whirlwind', 'talent(3,1)&player.buff(Wrecking Ball)&player.buff(Enrage)', 'target'},
+	{'Execute', 'talent(6,3)||{!talent(6,3)&player.rage>50}', 'target'},
+	{'Bloodthirst', '!buff(Enrage)', 'player'},
+	{'Raging Blow', '!player.buff(Enrage)', 'target'},
+	{'Execute', nil, 'target'},
+	{'Raging Blow', nil, 'target'},
+	{'Bloodthirst', nil, 'target'},
+	{'Furious Slash', nil, 'target'},
 	{Bladestorm},
-	{'Bloodbath', 'talent(6,1)&{player.buff(Frothing Berserker)||{player.rage>80&!talent(5,2)}}'}
+	{'Bloodbath', 'player.buff(Frothing Berserker)||{player.rage>80&!talent(5,2)}', 'target'}
 }
 
 local TwoTargets = {
-	{'Whirlwind', '!player.buff(Meat Cleaver)'},
 	{Bladestorm},
-	{'Rampage', '!player.buff(Enrage)||{player.rage==100&!player.buff(Juggernaut)}||player.buff(Massacre)'},
+	{'Rampage', '!buff(Enrage)||{rage==100&!buff(Juggernaut)}||.buff(Massacre)', 'player'},
 	{'Bloodthirst', '!player.buff(Enrage)'},
-	{'Raging Blow', 'talent(6,3)&player.area(8).enemies==2'},
-	{'Whirlwind', 'player.area(8).enemies>2'},
-	{'Dragon Roar'},
-	{'Bloodthirst'},
-	{'Whirlwind'}
+	{'Raging Blow', 'talent(6,3)', 'target'},
+	{'Dragon Roar', nil, 'target'},
+	{'Bloodthirst', nil, 'target'},
+	{'Whirlwind', nil, 'target'}
 }
 
 local inCombat = {
 	{Keybinds},
 	{Interrupts, 'target.interruptAt(70)&toggle(Interrupts)'},
+	{Interrupts_Random},
 	{Survival, 'player.health<100'},
-	{Cooldowns, 'toggle(Cooldowns)&target.inMelee'},
+	{Cooldowns, 'toggle(Cooldowns)&target.range<=5'},
 	{TwoTargets, 'player.area(8).enemies==2||player.area(8).enemies==3'},
 	{AoE, 'player.area(8).enemies>3'},
-	{ST, 'target.inMelee&target.inFront'},
+	{ST, 'player.area(8).enemies<=3&target.range<=5&target.inFront'},
 	{Ranged, '!target.inMelee&target.inFront'},
 	{Fel_Explosives, 'range<=5'}
 }
@@ -135,6 +150,7 @@ local inCombat = {
 local outCombat = {
 	{Keybinds},
 	{Interrupts, 'target.interruptAt(70)&toggle(Interrupts)'},
+	{Interrupts_Random}
 }
 
 NeP.CR:Add(72, {
