1910222856acb738ecf142f907c75cafd98741e9
Andreas Nilsen <adde88@gmail.com>
2017-09-30 19:17:13 +0200

    - Fix Ferocious Bite - Add Pre-pot - Clean CR a bit - Re-add upvalues

--- rotations/druid/feral.lua
+++ rotations/druid/feral.lua
@@ -1,4 +1,7 @@
 local _, Zylla = ...
+local unpack = _G.unpack
+local NeP = _G.NeP
+local Mythic_Plus = _G.Mythic_Plus
 
 local GUI = {
 	unpack(Zylla.Logo_GUI),
@@ -16,13 +19,17 @@ local GUI = {
 	{type = 'ruler'},	 	{type = 'spacer'},
 	-- Settings
 	{type = 'header', 	size = 16, text = 'Class Settings',												align = 'center'},
-	{type = 'checkspin',text = 'Light\'s Judgment - Units', 											key = 'LJ',					spin = 4,	step = 1,	max = 20,	check = true,	desc = Zylla.ClassColor..'World Spell usable on Argus.|r'},
-	{type = 'checkbox', text = 'Use Trinket #1', 																	key = 'trinket1',		default = true},
-	{type = 'checkbox', text = 'Use Trinket #2', 																	key = 'trinket2', 	default = true,		desc = Zylla.ClassColor..'Trinkets will be used whenever possible!|r'},
+	{type = 'checkbox', text = 'Enable DBM Integration',													key = 'kDBM', 			default = true},
+	{type = 'checkbox', text = 'Enable \'pre-potting\' and Flasks',								key = 'prepot', 		default = false},
+	{type = 'combo',		default = '3',																						key = 'list', 			list = Zylla.prepots, 	width = 175},
+	{type = 'spacer'},	{type = 'spacer'},
+	{type = 'checkspin',text = 'Light\'s Judgment - Units', 											key = 'LJ',					spin = 4,	step = 1,	max = 20, min = 1,	check = true,	desc = Zylla.ClassColor..'World Spell usable on Argus.|r'},
+	{type = 'checkbox', text = 'Use Trinket #1', 																	key = 'trinket1',		default = false},
+	{type = 'checkbox', text = 'Use Trinket #2', 																	key = 'trinket2', 	default = false,		desc = Zylla.ClassColor..'Trinkets will be used whenever possible!|r'},
 	{type = 'ruler'},	 	{type = 'spacer'},
 	-- Survival
 	{type = 'header', 	size = 16, text = 'Survival',															align = 'center'},
-	{type = 'checkspin',text = 'Swiftmend below HP%', 														key = 'swiftm', 		spin = 85, step = 5, max = 100, check = true},
+	{type = 'checkspin',text = 'Swiftmend', 																			key = 'swiftm', 		spin = 85, step = 5, max = 100, check = true},
 	{type = 'checkspin',text = 'Survival Instincts', 															key = 'sint', 			spin = 50, step = 5, max = 100, check = true},
 	{type = 'ruler'},		{type = 'spacer'},
 	unpack(Zylla.Mythic_GUI),
@@ -65,8 +72,14 @@ local PreCombat = {
 	{'Travel Form', 'toggle(xFORM)&movingfor>0.75&!indoors&!buff&!buff(Prowl)', 'player'},
 	{'!Regrowth', 'talent(7,2)&target.enemy&target.alive&!buff(Prowl)&!lastcast(Regrowth)&buff(Bloodtalons).stack<2', 'player'},
 	{'Cat Form', 'toggle(xFORM)&movingfor>0.75&indoors&!buff&!buff(Travel Form)&!buff(Prowl)', 'player'},
-	{'Prowl', 'toggle(xFORM)&toggle(xStealth)&!buff', 'player'},
-	{'Rake', 'player.buff(Prowl)&inMelee&inFront', 'target'}
+	{'Prowl', 'toggle(xFORM)&toggle(xStealth)&!buff&{!buff(Travel Form)||{buff(Travel Form)&target.enemy&target.range<=25}}', 'player'},
+	{'Rake', 'player.buff(Prowl)&inMelee&inFront&enemy&alive', 'target'},
+	-- Pots
+	{'#127844', 'UI(list)==1&item(127844).usable&item(127844).count>0&UI(kDBM)&UI(prepot)&!buff(Potion of the Old War)&dbm(pull in)<3', 'player'}, 			--XXX: Potion of the Old War
+	{'#127843', 'UI(list)==2&item(127843).usable&item(127843).count>0&UI(kDBM)&UI(prepot)&!buff(Potion of Deadly Grace)&dbm(pull in)<3', 'player'}, 		--XXX: Potion of Deadly Grace
+	{'#142117', 'UI(list)==3&item(142117).usable&item(142117).count>0&UI(kDBM)&UI(prepot)&!buff(Potion of Prolonged Power)&dbm(pull in)<3', 'player'},	--XXX: Potion of Prolonged Power
+	-- Flasks
+	{'#127848', 'item(127848).usable&item(127848).count>0&UI(prepot)&!buff(Flask of the Seventh Demon)', 'player'},	--XXX: Flask of the Seventh Demon
 }
 
 local Keybinds = {
@@ -75,20 +88,26 @@ local Keybinds = {
 }
 
 local Interrupts = {
-	{'!Skull Bash', 'player.form~=0&range<14&inFront', 'target'},
-	{'!Typhoon', 'player.spell(Skull Bash).cooldown>gcd&range<16&inFront', 'target'},
-	{'!Mighty Bash', 'player.spell(Skull Bash).cooldown>gcd&inMelee&inFront', 'target'}
-}
-
-local Interrupts_Random = {
-	{'!Skull Bash', 'interruptAt(70)&player.form>0&toggle(xIntRandom)&toggle(Interrupts)&inFront&range<14', 'enemies'},
-	{'!Typhoon', 'interruptAt(60)&toggle(xIntRandom)&toggle(Interrupts)&player.area(15).enemies.inFront>=1&player.spell(Skull Bash).cooldown>gcd', 'enemies'},
-	{'!Mighty Bash', 'interruptAt(70)&toggle(xIntRandom)&toggle(Interrupts)&inMelee&inFront&player.spell(Skull Bash).cooldown>gcd', 'enemies'}
+	{'!Skull Bash', 'player.form~=0&range<14'},
+	{'!Typhoon', 'interruptAt(60)&player.spell(Skull Bash).cooldown>gcd&range<16'},
+	{'!Mighty Bash', 'inMelee&player.spell(Skull Bash).cooldown>gcd'}
 }
 
 local Bear_Healing = {
-	{'Bear Form', 'form~1', 'player'},
+	{'Bear Form', 'form~=1', 'player'},
 	{'Frenzied Regeneration', nil, 'player'}
+}
+
+local Survival = {
+	{Bear_Healing, 'talent(3,2)&player.incdmg(5)>player.health.max*0.20&!player.buff(Frenzied Regeneration)'},
+	--{'/run CancelShapeshiftForm()', 'form>0&talent(3,3)&!player.buff(Rejuvenation)'},
+	--{'Rejuvenation', 'talent(3,3)&!player.buff(Rejuvenation)', 'player'},
+	{'/run CancelShapeshiftForm()', 'cooldown(Swiftmend).up.&form>0&talent(3,3)&player.health<=UI(swiftm_spin)&UI(swiftm_check)'},
+	{'Swiftmend', 'talent(3,3)&health<=UI(swiftm_spin)&UI(swiftm_check)', 'player'},
+	{'Survival Instincts', 'player.health<=UI(sint_spin)&UI(sint_check)', 'player'},
+	{'#152615', 'item(152615).usable&item(152615).count>0&health<=UI(AHP_spin)&UI(AHP_check)', 'player'}, 													-- Astral Healing Potion
+	{'#127834', 'item(152615).count==0&item(127834).usable&item(127834).count>0&health<=UI(AHP_spin)&UI(AHP_check)', 'player'}, 		-- Ancient Healing Potion
+	{'#5512', 'item(5512).usable&item(5512).count>0&health<=UI(HS_spin)&UI(HS_check)', 'player'}, 																	-- Health Stone
 }
 
 local SBT_Opener = {
@@ -103,11 +122,10 @@ local Cooldowns = {
 	{'Berserk', 'buff(Tiger\'s Fury)', 'player'},
 	{'Incarnation: King of the Jungle', '{spell(Tiger\'s Fury).cooldown<gcd||{energy.time_to_max>1&energy>25}}', 'player'},
 	{'Tiger\'s Fury', '{!buff(Clearcasting)&energy.deficit>50}||energy.deficit>70', 'player'},
-	{'Ferocious Bite', '!player.combopoints==0&dot(Rip).ticking&dot(Rip)remains<3&ttd>3&{health<25||talent(6,1)}', 'target'},
+	{'Ferocious Bite', 'player.combopoints>0&dot(Rip).ticking&dot(Rip)remains<3&ttd>3&{health<25||talent(6,1)}', 'target'},
 	{'!Regrowth', 'talent(7,2)&buff(Predatory Swiftness)&{player.combopoints>4||buff(Predatory Swiftness).remains<1.5||{talent(7,2)&combopoints==2&!buff(Bloodtalons)&spell(Ashamane\'s Frenzy).cooldown<gcd}}', 'player'},
 	{SBT_Opener, 'talent(6,1)&xtime<20'},
-	--# Special logic for Ailuro Pouncers legendary.
-	{'!Regrowth', 'equipped(137024)&talent(7,2)&buff(Predatory Swiftness).stack>1&!buff(Bloodtalons)', 'player'},
+	{'!Regrowth', 'equipped(137024)&talent(7,2)&buff(Predatory Swiftness).stack>1&!buff(Bloodtalons)', 'player'},		--XXX: Special logic for Ailuro Pouncers legendary.
 	{'#trinket1', 'UI(trinket1)'},
 	{'#trinket2', 'UI(trinket2)'},
 	{'Light\'s Judgment', 'UI(LJ_check)&range<61&area(15).enemies>=UI(LJ_spin)', 'enemies.ground'}
@@ -120,7 +138,7 @@ local Finisher = {
 	{'Swipe', 'player.combopoints==5&{player.area(8).enemies.inFront>5||{player.area(8).enemies.inFront>2&!talent(7,2)}}&player.combopoints==5&{energy.time_to_max<1||player.buff(Berserk)||player.buff(Incarnation: King of the Jungle)||player.spell(Tiger\'s Fury).cooldown<3||{talent(7,3)&player.buff(Clearcasting)}}', 'target'},
 	{'Swipe', 'player.area(8).enemies.inFront>7', 'target'},
 	{'Rip', '{!dot.ticking||{dot.remains<8&health>25&!talent(6,1)}||persistent_multiplier>dot.pmultiplier}&{dot.remains>dot.tick_time*4&player.combopoints==5}&{energy.time_to_max<1||player.buff(Berserk)||player.buff(Incarnation: King of the Jungle)||player.spell(Tiger\'s Fury).cooldown<3||{talent(6,2)&player.buff(Clearcasting)}||talent(5,1)||!dot.ticking||{dot(Rake).remains<1.5&player.area(8).enemies.inFront<6}}', 'target'},
-	{'Ferocious Bite', 'energy.deficit==0&player.combopoints==5&{energy.time_to_max<1||player.buff(Berserk)||player.buff(Incarnation: King of the Jungle)||player.spell(Tiger\'s Fury).cooldown<3||{talent(7,3)&player.buff(Clearcasting)}}', 'target'}
+	{'Ferocious Bite', '	player.combopoints==5&{energy.time_to_max<1||player.buff(Berserk)||player.buff(Incarnation: King of the Jungle)||player.spell(Tiger\'s Fury).cooldown<3||{talent(7,3)&player.buff(Clearcasting)}}', 'target'}
 }
 
 local Generator = {
@@ -142,26 +160,14 @@ local xCombat = {
 	{Generator}
 }
 
-local Survival = {
-	{Bear_Healing, 'talent(3,2)&player.incdmg(5)>player.health.max*0.20&!player.buff(Frenzied Regeneration)'},
-	--{'/run CancelShapeshiftForm()', 'form>0&talent(3,3)&!player.buff(Rejuvenation)'},
-	--{'Rejuvenation', 'talent(3,3)&!player.buff(Rejuvenation)', 'player'},
-	{'/run CancelShapeshiftForm()', 'cooldown(Swiftmend).up.&form>0&talent(3,3)&player.health<=UI(swiftm_spin)&UI(swiftm_check)'},
-	{'Swiftmend', 'talent(3,3)&health<=UI(swiftm_spin)&UI(swiftm_check)', 'player'},
-	{'Survival Instincts', 'player.health<=UI(sint_spin)&UI(sint_check)', 'player'},
-	{'#152615', 'item(152615).usable&item(152615).count>0&health<=UI(AHP_spin)&UI(AHP_check)', 'player'}, 													-- Astral Healing Potion
-	{'#127834', 'item(152615).count==0&item(127834).usable&item(127834).count>0&health<=UI(AHP_spin)&UI(AHP_check)', 'player'}, 		-- Ancient Healing Potion
-	{'#5512', 'item(5512).usable&item(5512).count>0&health<=UI(HS_spin)&UI(HS_check)', 'player'}, 																	-- Health Stone
-}
-
 local inCombat = {
 	{Keybinds},
-	{Interrupts, 'target.interruptAt(70)&toggle(Interrupts)&target.inFront&target.inMelee'},
-	{Interrupts_Random},
+	{Interrupts, 'interruptAt(70)&toggle(Interrupts)&inFront', 'target'},
+	{Interrupts, 'interruptAt(70)&toggle(xIntRandom)&toggle(Interrupts)&inFront', 'enemies'},
 	{Survival, 'player.health<100'},
 	{'Cat Form', '!buff(Frenzied Regeneration)&{!buff(Cat Form)&{!buff(Travel Form)||area(8).enemies.inFront>0}}', 'player'},
 	{Cooldowns, '!player.buff(Frenzied Regeneration)&toggle(Cooldowns)'},
-	{'Moonfire', 'talent(1,3)&!range>8&range<41&inFront&!player.buff(Prowl)&!debuff(Moonfire)', 'target'},
+	{'Moonfire', 'talent(1,3)&!inMelee&range<41&inFront&!player.buff(Prowl)&!debuff', 'target'},
 	{Mythic_Plus, 'inMelee&inFront'},
 	{xCombat, '!player.buff(Frenzied Regeneration)&target.inMelee&target.inFront'}
 }
@@ -169,8 +175,6 @@ local inCombat = {
 local outCombat = {
 	{Keybinds},
 	{PreCombat},
-	{Interrupts, 'target.interruptAt(70)&toggle(Interrupts)'},
-	{Interrupts_Random}
 }
 
 NeP.CR:Add(103, {
@@ -179,7 +183,7 @@ NeP.CR:Add(103, {
 	ic = inCombat,
 	ooc = outCombat,
 	gui = GUI,
-	gui_st = {title='Zylla\'s Combat Routines', width='256', height='520', color='A330C9'},
+	gui_st = {title='Zylla\'s Combat Routines', width='256', height='760', color='A330C9'},
 	ids = Zylla.SpellIDs[Zylla.Class],
 	wow_ver = Zylla.wow_ver,
 	nep_ver = Zylla.nep_ver,
