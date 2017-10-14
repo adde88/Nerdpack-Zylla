411c28f2d142320a58349228a7da03ea206ed1c5
Zylla <adde88@gmail.com>
2017-10-14 15:11:19 +0200

    Vengeance Demon Hunter Updated.

--- rotations/demonhunter/vengeance.lua
+++ rotations/demonhunter/vengeance.lua
@@ -16,9 +16,16 @@ local GUI = {
 	unpack(Zylla.PayPal_GUI),
 	{type = 'spacer'},
 	unpack(Zylla.PayPal_IMG),
-	{type = 'ruler'},	 		{type = 'spacer'},
+	{type = 'spacer'},		{type = 'ruler'},	 	{type = 'spacer'},
+	--TODO: Targetting: Use, or NOT use?! We'll see....
+	{type = 'header', 		size = 16, text = 'Targetting:',															align = 'center'},
+	{type = 'combo',			default = 'normal',																						key = 'target', 					list = Zylla.faketarget, 	width = 75},
+	{type = 'spacer'},
+	{type = 'text', 			text = Zylla.ClassColor..'Only one can be enabled.\nChose between normal targetting, or hitting the highest/lowest enemy.|r'},
+	{type = 'spacer'},		{type = 'ruler'},	 	{type = 'spacer'},
 	-- Settings
 	{type = 'header', 		size = 16, text = 'Class Settings',														align = 'center'},
+	{type = 'spinner',		size = 10, text = 'Interrupt at percentage:', 														key = 'intat',				default = 60,	step = 5, shiftStep = 10,	max = 100, min = 1},
 	{type = 'checkbox', 	text = 'Enable DBM Integration',															key = 'kDBM', 				default = true},
 	{type = 'checkbox', 	text = 'Enable \'pre-potting\' and Flasks',										key = 'prepot', 			default = false},
 	{type = 'combo',			default = "1",																								key = "list", 				list = Zylla.prepots, 	width = 175},
@@ -32,9 +39,11 @@ local GUI = {
 	-- Survival
 	{type = 'header', 		text = 'Survival',									  	      								align = 'center'},
 	{type = 'spinner', 		text = 'Soul Cleave',               													key = 'SC_HP',    		default = 85, max = 100, min = 1, step = 5, shiftStep = 10,},
-	{type = 'checkspin', 	text = 'Soul Barrier - Soul Fragments',               				key = 'sb',    				spin = 4,  max = 10,  min = 1, step = 1, shiftStep = 2,  check = true},
-  {type = 'checkspin',	text = 'Healthstone',																					key = 'HS',						spin = 45, max = 100, min = 1, step = 5, shiftStep = 10, check = true},
-  {type = 'checkspin',	text = 'Healing Potion',																			key = 'AHP',					spin = 45, max = 100, min = 1, step = 5, shiftStep = 10, check = true},
+	{type = 'checkspin', 	text = 'Soul Barrier - Soul Fragments',               				key = 'sb',    				spin = 4,  		max = 10,  min = 1, step = 1, shiftStep = 2,  check = true},
+	{type = 'spinner', 		text = 'Soul Barrier - Health Threshold',               			key = 'sbhp',    			default = 60, max = 100, min = 1, step = 5, shiftStep = 10, check = true},
+	{type = 'checkspin', 	text = 'Metamorphosis'								,               				key = 'meta',  				spin = 15, 		max = 100, min = 1, step = 5, shiftStep = 10, check = true},
+  {type = 'checkspin',	text = 'Healthstone',																					key = 'HS',						spin = 45, 		max = 100, min = 1, step = 5, shiftStep = 10, check = true},
+  {type = 'checkspin',	text = 'Healing Potion',																			key = 'AHP',					spin = 45, 		max = 100, min = 1, step = 5, shiftStep = 10, check = true},
 	{type = 'ruler'},	  	{type = 'spacer'},
 	unpack(Zylla.Mythic_GUI),
 }
@@ -67,97 +76,95 @@ end
 local PreCombat = {
 	{'%pause', 'player.buff(Shadowmeld)'},
 	-- Pots
-	{'#127844', 'UI(list)==1&item(127844).usable&item(127844).count>0&UI(kDBM)&UI(prepot)&!buff(Potion of the Old War)&dbm(pull in)<3', 'player'}, 	--XXX: Potion of the Old War
-	{'#127843', 'UI(list)==2&item(127843).usable&item(127843).count>0&UI(kDBM)&UI(prepot)&!buff(Potion of Deadly Grace)&dbm(pull in)<3', 'player'}, 	--XXX: Potion of Deadly Grace
-	{'#142117', 'UI(list)==3&item(142117).usable&item(142117).count>0&UI(kDBM)&UI(prepot)&!buff(Potion of Prolonged Power)&dbm(pull in)<3', 'player'}, 	--XXX: Potion of Prolonged Power
+	{'#127844', 'UI(list)==1&item(127844).usable&item(127844).count>0&UI(kDBM)&UI(prepot)&!buff(Potion of the Old War)&dbm(pull in)<3'}, 			--XXX: Potion of the Old War
+	{'#127843', 'UI(list)==2&item(127843).usable&item(127843).count>0&UI(kDBM)&UI(prepot)&!buff(Potion of Deadly Grace)&dbm(pull in)<3'}, 		--XXX: Potion of Deadly Grace
+	{'#142117', 'UI(list)==3&item(142117).usable&item(142117).count>0&UI(kDBM)&UI(prepot)&!buff(Potion of Prolonged Power)&dbm(pull in)<3'}, 	--XXX: Potion of Prolonged Power
 	-- Flasks
-	{'#127848', 'item(127848).usable&item(127848).count>0&UI(prepot)&!buff(Flask of the Seventh Demon)', 'player'},	--XXX: Flask of the Seventh Demon
+	{'#127848', 'item(127848).usable&item(127848).count>0&UI(prepot)&!buff(Flask of the Seventh Demon)'},	--XXX: Flask of the Seventh Demon
+	{'#153023', 'item(153023).usable&item(153023).count>0&UI(prepot)&!buff(Defiled Augmentation)'},				--XXX: Lightforged Augment Rune
 }
 
 local Keybinds = {
 	{'%pause', 'keybind(lshift)&UI(lshift)'},
 	{'Sigil of Flame', 'keybind(lalt)', 'cursor.ground'},
-	{'Infernal Strike', 'keybind(lcontrol)', 'cursor.ground'},
+	{'&Infernal Strike', 'keybind(lcontrol)', 'cursor.ground'},
 }
 
 local Interrupts = {
-	{'!Consume Magic', 'target.interruptAt(70)&target.inFront&target.inMelee'},
-	{'!Sigil of Misery', 'advanced&target.interruptAt(1)&target.range<31&spell(Consume Magic).cooldown>gcd&!prev_gcd(Consume Magic)', 'target.ground'},
-	{'!Sigil of Silence', 'advanced&target.interruptAt(5)&target.range<31&spell(Sigil of Misery).cooldown>gcd&spell(Consume Magic).cooldown>gcd&!prev_gcd(Consume Magic)', 'target.ground'},
-	{'!Arcane Torrent', 'target.interruptAt(70)&target.inFront&target.inMelee&spell(Consume Magic).cooldown>gcd&!prev_gcd(Consume Magic)'},
-}
-
-local Interrupts_Random = {
-	{'!Consume Magic', 'interruptAt(70)&toggle(xIntRandom)&toggle(Interrupts)&inFront&inMelee', 'enemies'},
-	{'!Sigil of Misery', 'advanced&interruptAt(1)&toggle(xIntRandom)&toggle(Interrupts)&player.spell(Consume Magic).cooldown>gcd&!prev_gcd(Consume Magic)&range<31', 'enemies.ground'},
-	{'!Sigil of Silence', 'advanced&interruptAt(5)&toggle(xIntRandom)&toggle(Interrupts)&player.spell(Sigil of Misery).cooldown>gcd&player.spell(Consume Magic).cooldown>gcd&!prev_gcd(Consume Magic)&range<31', 'enemies.ground'},
-	{'!Arcane Torrent', 'interruptAt(70)&toggle(xIntRandom)&toggle(Interrupts)&player.spell(Consume Magic).cooldown>gcd&!prev_gcd(Consume Magic)&inMelee'},
+	{'&Consume Magic', 'inFront&inMelee'},
+	{'!Sigil of Misery', 'advanced&interruptAt(5)&range<=30&spell(Consume Magic).cooldown>=gcd&!player.lastgcd(Consume Magic)', 'target.ground'},
+	{'!Sigil of Misery', 'toggle(xIntRandom)&advanced&interruptAt(5)&range<=30&spell(Consume Magic).cooldown>=gcd&!player.lastgcd(Consume Magic)', 'enemies.ground'},
+	{'!Sigil of Silence', 'advanced&interruptAt(5)&range<=30&spell(Sigil of Misery).cooldown>=gcd&spell(Consume Magic).cooldown>=gcd&!player.lastgcd(Consume Magic)', 'target.ground'},
+	{'!Sigil of Silence', 'toggle(xIntRandom)&advanced&interruptAt(5)&ange<=30&spell(Sigil of Misery).cooldown>=gcd&spell(Consume Magic).cooldown>=gcd&!player.lastgcd(Consume Magic)', 'enemies.ground'},
+	{'!Arcane Torrent', 'inFront&inMelee&spell(Consume Magic).cooldown>=gcd&!player.lastgcd(Consume Magic)'},
 }
 
 local Survival ={
-	{'#152615', 'item(152615).usable&item(152615).count>0&health<=UI(AHP_spin)&UI(AHP_check)', 'player'}, 													--XXX: Astral Healing Potion
-	{'#127834', 'item(152615).count==0&item(127834).usable&item(127834).count>0&health<=UI(AHP_spin)&UI(AHP_check)', 'player'}, 		--XXX: Ancient Healing Potion
-	{'#5512', 'item(5512).usable&item(5512).count>0&health<=UI(HS_spin)&UI(HS_check)', 'player'}, 																	--XXX: Health Stone
+	{'#152615', 'item(152615).usable&item(152615).count>0&health<=UI(AHP_spin)&UI(AHP_check)'}, 													--XXX: Astral Healing Potion
+	{'#127834', 'item(152615).count==0&item(127834).usable&item(127834).count>0&health<=UI(AHP_spin)&UI(AHP_check)'}, 		--XXX: Ancient Healing Potion
+	{'#5512', 'item(5512).usable&item(5512).count>0&health<=UI(HS_spin)&UI(HS_check)'}, 																	--XXX: Health Stone
 }
 
 local xTaunts = {
-	{'Torment', 'combat&alive&threat<100&range<=30', 'enemies'},
+	{'Torment', 'combat&alive&threat<100&range<=30'},
 }
 
-local Mitigations = {
-	{'Metamorphosis', 'toggle(cooldowns)&!player.buff(Demon Spikes)&!target.debuff(Fiery Brand)&!player.buff(Metamorphosis)&player.incdmg(1)>=player.health.max*0.50'},
-	{'Fiery Brand', '!player.buff(Demon Spikes)&!player.buff(Metamorphosis)', 'target'},
-	{'!Demon Spikes', 'player.spell(Demon Spikes).charges>0&!player.buff(Demon Spikes)&!target.debuff(Fiery Brand)&!player.buff(Metamorphosis)'},
-	{'!Empower Wards', 'target.casting.percent>80', 'player'},
-	{'!Soul Barrier', 'buff(Soul Fragments).count>UI(sb_spin&UI(sb_check))', 'player'},
-}
-
-local xCombat = {
+local Cooldowns = {
 	{'#trinket1', 'UI(trinket1)'},
 	{'#trinket2', 'UI(trinket2)'},
 	{'Light\'s Judgment', 'UI(LJ_check)&range<61&area(15).enemies>UI(LJ_spin)', 'enemies'},
-	{'Spirit Bomb', 'count(Frailty).enemies.debuffs==0&player.buff(Soul Fragments).count>0', 'target'},
-	{'Immolation Aura'},
-	{'Felblade', 'player.pain<81', 'target'},
-	{'Fel Devastation', 'player.incdmg(1)>=player.health.max*0.25'},
-	{'Soul Cleave', 'player.pain>69||player.incdmg(1)>=player.health.max*0.25', 'target'},
-	{'Soul Carver', nil, 'target'},
-	{'Fel Eruption', nil, 'target'},
-	{'Shear', nil, 'target'},
-	{'Fracture', 'player.pain>49||player.buff(Soul Fragments).count<5', 'target'},
-	{'Infernal Strike', 'advanced&UI(kIS)&talent(3,2)&target.debuff(Sigil of Flame).duration<gcd&player.spell(Sigil of Flame).cooldown>gcd*3&player.spell(Infernal Strike).charges>0', 'target.ground'}, -- Uses Infernal Strike automatically when you have the appropriate talent, can be disabled.
-	{'Infernal Strike', '!advanced&UI(kIS)&talent(3,2)&target.debuff(Sigil of Flame).duration<gcd&player.spell(Sigil of Flame).cooldown>gcd*3&player.spell(Infernal Strike).charges>0', 'player.ground'}, -- Uses Infernal Strike automatically when you have the appropriate talent, can be disabled.
+	{'&#144259', 'UI(kj_check)&range<41&area(10).enemies>=UI(kj_spin)&equipped(144259)'}, --XXX: Kil'jaeden's Burning Wish (Legendary)
 }
 
-local Ranged = {
-	{'Throw Glaive', 'toggle(aoe)&!inMelee&range<31&inFront', 'target'},
-	{'Sigil of Chains', 'advanced&range<31&area(8).enemies>2&combat', 'enemies.ground'},
-	{'Sigil of Flame', 'toggle(aoe)&advanced&range<31&!target.debuff(Sigil of Flame)', 'target.ground'},
-	{'Sigil of Flame', '!advanced&range<31&!target.debuff(Sigil of Flame)', 'player.ground'},
+local Mitigations = {
+	{'!Metamorphosis', 'toggle(cooldowns)&UI(meta_check)&!buff(Demon Spikes)&!target.debuff(Fiery Brand)&!buff&{{health<75&incdmg(1)>=health.max*0.50}||health<=UI(meta_spin)}'},
+	{'!Demon Spikes', 'player.pain>21&spell.charges>0&!buff&!target.debuff(Fiery Brand)&!buff(Metamorphosis)'},
+	{'!Empower Wards', 'incdmg(3).magic>player.health.max*0.1&health<75'},
+	{'!Soul Barrier', 'buff(Soul Fragments).count>UI(sb_spin)&UI(sb_check)&health<=UI(sbhp)'},
+	{'Immolation Aura', 'area(8).enemies>0'},
+}
+
+local xCombat = {
+	{Cooldowns},
+	{Interrupts, '@Zylla.InterruptAt(intat)&toggle(Interrupts)'},
+	{Interrupts, '@Zylla.InterruptAt(intat)&toggle(Interrupts)&toggle(xIntRandom)', 'enemies'},
+	{'Fiery Brand', 'inFront&inMelee&!player.buff(Demon Spikes)&!player.buff(Metamorphosis)'},
+	{'Soul Carver', 'inFront&inMelee&{spell(Fiery Brand).cooldown>50||player.incdmg(3)>player.health.max*0.2}'},
+	{'Shear', 'inFront&inMelee&player.pain<90'},
+	{'Sever', 'inFront&inMelee'},
+	{'Fracture', 'inFront&inMelee&player.buff(Soul Fragments).count<=4'},
+	{'Spirit Bomb', 'inFront&inMelee&count(Frailty).enemies.debuffs==0&player.buff(Soul Fragments).count>=4'},
+	{'Fel Devastation', 'inFront&inMelee&player.incdmg(1)>=player.health.max*0.25'},
+	{'Soul Cleave', 'inFront&inMelee&{{player.pain>=60}||{player.health<UI(SC_HP)}}'},
+	{'Fel Eruption', 'inFront&inMelee'},
+	{'Felblade', 'inFront&inMelee'},
+	{'&Infernal Strike', 'advanced&UI(kIS)&inFront&inMelee&talent(3,2)&debuff(Sigil of Flame).duration<=gcd&spell(Sigil of Flame).cooldown>=gcd*3&spell(Infernal Strike).charges>0', 'target.ground'},
+	{'Throw Glaive', 'toggle(aoe)&!inMelee&range<=30&inFront'},
+	{'Sigil of Chains', 'toggle(aoe)&advanced&range<=30&area(8).enemies>=3&combat&alive', 'enemies.ground'},
+	{'Sigil of Flame', 'toggle(aoe)&advanced&range<=30&!debuff', 'target.ground'},
 }
 
 local inCombat = {
 	{Keybinds},
-	{Interrupts_Random},
-	{Interrupts, 'toggle(Interrupts)'},
-	{xTaunts, 'toggle(super_taunt)'},
-	{Ranged},
-	{Mitigations},
-  {Survival},
+	{xTaunts, 'toggle(super_taunt)', 'enemies'},
+  {Survival, nil, 'player'},
+	{Mitigations, nil, 'player'},
 	{Mythic_Plus, 'inMelee'},
-	{xCombat, 'target.inFront&target.inMelee'},
-	{'#144259', 'UI(kj_check)&range<41&area(10).enemies>=UI(kj_spin)&equipped(144259)', 'target'}, --XXX: Kil'jaeden's Burning Wish (Legendary)
+	{xCombat, 'UI(target)==normal', 'target'},
+	{xCombat, 'combat&alive&UI(target)==highest', 'highestenemy'},
+	{xCombat, 'combat&alive&UI(target)==lowest', 'lowestenemy'},
+	{xCombat, 'combat&alive&UI(target)==nearest', 'nearestenemy'},
+	{xCombat, 'combat&alive&UI(target)==furthestenemy', 'furthestenemy'},
 }
 
 local outCombat = {
-	{PreCombat},
-	{Keybinds},
-	{Interrupts_Random},
-	{Interrupts, 'toggle(Interrupts)'},
+		{PreCombat, nil, 'player'},
+		{Keybinds},
 }
 
 NeP.CR:Add(581, {
 	name = '[|cff'..Zylla.addonColor..'Zylla\'s|r] Demon Hunter - Vengeance',
+	pooling = true,
 	ic = inCombat,
 	ooc = outCombat,
 	gui = GUI,
