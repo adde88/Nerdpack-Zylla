91281ee5c0d23f43cd86e839953903df3b494ff4
Zylla <adde88@gmail.com>
2017-10-14 15:14:55 +0200

    Demonology Warlock Updated.

--- rotations/warlock/demonology.lua
+++ rotations/warlock/demonology.lua
@@ -11,32 +11,42 @@ local GUI = {
 	{type = 'checkbox',	text = 'Left Ctrl: '..Zylla.ClassColor..'Shadowfury|r',			align = 'left', 			key = 'lcontrol',	default = true},
 	{type = 'checkbox',	text = 'Left Alt: '..Zylla.ClassColor..'Demonic Circle|r',	align = 'left', 			key = 'lalt', 		default = true},
 	{type = 'checkbox',	text = 'Right Alt: '..Zylla.ClassColor..'|r',								align = 'left', 			key = 'ralt', 		default = true},
+	{type = 'checkbox', size = 10, text = Zylla.ClassColor..'Summon Pets                                  Select Pet: |r',		align = 'right', 	key = 'epets', 		default = true},
 	{type = 'spacer'},
---{type = 'checkbox', text = 'Enable Chatoverlay', 																key = 'chat', 				width = 55, 			default = true, desc = Zylla.ClassColor..'This will enable some messages as an overlay!|r'},
+	{type = 'checkbox', text = 'Enable Chatoverlay', 																key = 'chat', 				width = 55, 			default = true, desc = Zylla.ClassColor..'This will enable some messages as an overlay!|r'},
 	unpack(Zylla.PayPal_GUI),
 	{type = 'spacer'},
 	unpack(Zylla.PayPal_IMG),
-	{type = 'ruler'},	 	{type = 'spacer'},
+	{type = 'spacer'},	{type = 'ruler'},	 	{type = 'spacer'},
+	--TODO: Targetting: Use, or NOT use?! We'll see....
+	{type = 'header', 	size = 16, text = 'Targetting:',														align = 'center'},
+	{type = 'combo',		default = 'normal',																					key = 'target', 					list = Zylla.faketarget, 	width = 75},
+	{type = 'spacer'},
+	{type = 'text', 		text = Zylla.ClassColor..'Only one can be enabled.\nChose between normal targetting, or hitting the highest/lowest enemy.|r'},
+	{type = 'spacer'},	{type = 'ruler'},	 	{type = 'spacer'},
 	-- Settings
-	{type = 'header', 	text = 'Class Settings',																		align = 'center'},
+	{type = 'header', 	size = 16, text = 'Class Settings',													align = 'center'},
+	{type = 'spinner',	size = 11, text = 'Interrupt at percentage:', 							key = 'intat',				default = 60,	step = 5, shiftStep = 10,	max = 100, min = 1},
+	{type = 'checkbox', text = 'Enable DBM Integration',														key = 'kDBM', 				default = true},
+	{type = 'checkbox', text = 'Enable \'pre-potting\', flasks and Legion-rune',		key = 'prepot', 			default = false},
+	{type = 'combo',		default = '1',																							key = 'list', 				list = Zylla.prepots, 	width = 175},
+	{type = 'spacer'},	{type = 'spacer'},
+	{type = 'checkspin',text = 'Light\'s Judgment - Units', 												key = 'LJ',						spin = 4,	step = 1,	max = 20, min = 1,	check = true,	desc = Zylla.ClassColor..'World Spell usable on Argus.|r'},
 	{type = 'checkbox', text = 'Summon Pet',																				key = 'kPet',					default = true},
-	{type = 'ruler'},	  {type = 'spacer'},
+	{type = 'checkbox', text = 'Use Trinket #1', 																		key = 'trinket1',			default = false},
+	{type = 'checkbox', text = 'Use Trinket #2', 																		key = 'trinket2',		 	default = false,	desc = Zylla.ClassColor..'Trinkets will be used whenever possible!|r'},
+	{type = 'spacer'},
+	{type = 'checkspin', text = 'Kil\'Jaeden\'s Burning Wish - Units', 							key = 'kj', 					align = 'left', width = 55, step = 1, shiftStep = 2, spin = 4, max = 20, min = 0, check = true, desc = Zylla.ClassColor..'Legendary will be used only on selected amount of units!|r'},
+	{type = 'spacer'}, 	{type = 'ruler'},
 	-- Survival
-	{type = 'header',	text = 'Survival', 																						align = 'center'},
-	{type = 'checkbox',	text = 'Enable Unending Resolve', 													key = 'S_UEE', 				default = true},
-	{type = 'spinner',	text = '',																									key = 'S_UE', 				default = 40},
-	{type = 'checkbox', text = 'Enable Dark Pact', 																	key = 'S_DPE', 				default = true},
-	{type = 'spinner',	text = '',																									key = 'S_DP', 				default = 50},
-	{type = 'checkbox',	text = 'Enable Drain Life', 																key = 'S_DLE', 				default = true},
-	{type = 'spinner',	text = '',																									key = 'S_DL', 				default = 30},
-	{type = 'checkbox',	text = 'Enable Health Funnel', 															key = 'S_HFE', 				default = true},
-	{type = 'spinner',	text = '',																									key = 'S_HF', 				default = 60},
-	{type = 'checkbox',	text = 'Enable Gift of the Naaru', 													key = 'S_GOTNE', 			default = true},
-	{type = 'spinner',	text = '',																									key = 'S_GOTN', 			default = 40},
-	{type = 'checkbox',	text = 'Enable Healthstone', 																key = 'S_HSE', 				default = true},
-	{type = 'spinner',	text = '',																									key = 'S_HS', 				default = 20},
-	{type = 'checkbox',	text = 'Enable Ancient Healing Potion', 										key = 'S_AHPE', 			default = true},
-	{type = 'spinner',	text = '',																									key = 'S_AHP', 				default = 20},
+	{type = 'header',		size = 16, text = 'Survival', 															align = 'center'},
+	{type = 'checkspin',text = 'Unending Resolve',																	key = 'uer',  				align = 'left', width = 55, step = 5, shiftStep = 10, max = 20, min = 1, spin = 40, check = true},
+	{type = 'checkspin',text = 'Dark Pact',																					key = 'dp',  					align = 'left', width = 55, step = 5, shiftStep = 10, max = 20, min = 1, spin = 50, check = true},
+	{type = 'checkspin',text = 'Drain Life',																				key = 'dl',  					align = 'left', width = 55, step = 5, shiftStep = 10, max = 20, min = 1, spin = 30, check = true},
+	{type = 'checkspin',text = 'Healtn Funnel',																			key = 'hf',  					align = 'left', width = 55, step = 5, shiftStep = 10, max = 20, min = 1, spin = 60, check = true},
+	{type = 'checkspin',text = 'Gift of the Naaru',																	key = 'gotn',  				align = 'left', width = 55, step = 5, shiftStep = 10, max = 20, min = 1, spin = 45, check = true},
+	{type = 'checkspin',text = 'Healthstone',																				key = 'HS',  					align = 'left', width = 55, step = 5, shiftStep = 10, max = 20, min = 1, spin = 45, check = true},
+	{type = 'checkspin',text = 'Ancient Healing Potion', 														key = 'AHP',  				align = 'left', width = 55, step = 5, shiftStep = 10, max = 20, min = 1, spin = 40, check = true},
 	{type = 'ruler'},	{type = 'spacer'},
 	unpack(Zylla.Mythic_GUI),
 }
@@ -52,7 +62,6 @@ local exeOnLoad = function()
   print('|cffFFFB2F Configuration: |rRight-click MasterToggle and go to Combat Routines Settings!|r')
 
 	NeP.Interface:AddToggle({
-		-- Doom
 		key = 'Doom',
 		name = 'Doom',
 		text = 'Enable/Disable: Casting of Doom on targets',
@@ -68,37 +77,41 @@ local exeOnLoad = function()
 
 end
 
+local PreCombat = {
+	-- Pots
+	{'#127844', 'UI(list)==1&item(127844).usable&item(127844).count>0&UI(kDBM)&UI(prepot)&!buff(Potion of the Old War)&dbm(pull in)<5'}, 			--XXX: Potion of the Old War
+	{'#127843', 'UI(list)==2&item(127843).usable&item(127843).count>0&UI(kDBM)&UI(prepot)&!buff(Potion of Deadly Grace)&dbm(pull in)<5'}, 		--XXX: Potion of Deadly Grace
+	{'#142117', 'UI(list)==3&item(142117).usable&item(142117).count>0&UI(kDBM)&UI(prepot)&!buff(Potion of Prolonged Power)&dbm(pull in)<5'}, 	--XXX: Potion of Prolonged Power
+	-- Flasks
+	{'#127847', 'item(127847).usable&item(127847).count>0&UI(prepot)&!buff(Flask of the Whispered Pact)'},	--XXX:  Flask of the Whispered Pact
+	{'#153023', 'item(153023).usable&item(153023).count>0&UI(prepot)&!buff(Defiled Augmentation)'},					--XXX: Lightforged Augment Rune
+}
+
 local Survival = {
-	{'&Unending Resolve', 'UI(S_UEE)&player.health<=UI(S_UE)'},
-	{'&Dark Pact', 'UI(S_DPE)&talent(5,3)&pet.exists&player.health<=UI(S_DP)'},
-	{'&Gift of the Naaru', 'UI(S_GOTNE)&player.health<=UI(S_GOTN)'},
-	{'#127834', 'item(127834).count>0&player.health<UI(S_HS)'},        -- Ancient Healing Potion
-  {'#5512', 'item(5512).count>0&player.health<UI(S_AHP)', 'player'},  --Health Stone
+	{'&Unending Resolve', 'UI(uer_check)&player.health<=UI(uer_spin)'},
+	{'&Dark Pact', 'UI(dp_check)&talent(5,3)&pet.exists&player.health<=UI(dp_spin)'},
+	{'&Gift of the Naaru', 'UI(gotn_check)&player.health<=UI(gotn_spin)'},
+	{'#152615', 'item(152615).usable&item(152615).count>0&health<=UI(AHP_spin)&UI(AHP_check)'}, 													--XXX: Astral Healing Potion
+	{'#127834', 'item(152615).count==0&item(127834).usable&item(127834).count>0&health<=UI(AHP_spin)&UI(AHP_check)'}, 		--XXX: Ancient Healing Potion
+	{'#5512', 'item(5512).usable&item(5512).count>0&health<=UI(HS_spin)&UI(HS_check)'}, 																	--XXX: Health Stone
 }
 
 local Interrupts = {
-	{'!Shadowfury', 'target.range<41&target.inFront&!player.moving&UI(K_SF)&target.interruptAt(70)', 'target.ground'},
-	{'!Shadowfury', 'target.range<41&target.inFront&!player.moving&UI(K_SF)&target.area(10).enemies>2', 'target.ground'},
-	{'!Mortal Coil', 'target.range<30&target.inFront&target.interruptAt(70)', 'target'},
-	{'&89766', 'target.petrange<9&pet.exists&target.interruptAt(70)', 'target'},
-}
-
-local Interrupts_Random = {
-	{'!Shadowfury', 'inFront&range<40&!player.moving&interruptAt(70)&toggle(xIntRandom)&toggle(Interrupts)', 'enemies'},
-	{'!Shadowfury', 'inFront&range<40&!player.moving&toggle(xIntRandom)&toggle(Interrupts)&area(10).enemies>2', 'enemies'},
-	{'!Mortal Coil', 'inFront&range<31&toggle(xIntRandom)&toggle(Interrupts)&interruptAt(70)', 'enemies'},
-	{'&89766', 'petrange<9&pet.exists&interruptAt(70)', 'enemies'},
+	{'!Shadowfury', 'range<41&inFront&!player.moving&UI(K_SF)', {ground=true}},
+	{'!Shadowfury', 'range<41&inFront&!player.moving&UI(K_SF)&area(10).enemies>2', 'target.ground'},
+	{'!Mortal Coil', 'range<30&inFront'},
+	{'&89766', 'petrange<9&pet.exists'},
 }
 
 local Player = {
-	{'!Drain Life', 'UI(S_DLE)&player.health<=UI(S_DL)', 'target'},
-	{'!Health Funnel', 'UI(S_HFE)&pet.alive&pet.health<=UI(S_HF)', 'pet'},
+	{'!Drain Life', 'UI(dl_check)&player.health<=UI(dl_spin)'},
+	{'!Health Funnel', 'UI(hf_check)&pet.alive&health<=UI(hf_spin)', 'pet'},
 }
 
 local Keybinds = {
 	{'%pause', 'keybind(lshift)&UI(lshift)'},
 	{'!Shadowfury', '!player.moving&UI(K_SF)&talent(3,3)&keybind(lcontrol)', 'cursor.ground'},
-	{'!Demonic Circle', 'UI(K_DC)&talent(3,1)&keybind(lalt)'},
+	{'!Demonic Circle', 'UI(K_DC)&talent(3,1)&keybind(lalt)&UI(lalt)'},
 }
 
 local Cooldowns = {
@@ -120,13 +133,13 @@ local DW_Clip = {
 	{'!Summon Felguard', '!player.moving&!pet.exists&!talent(6,1)'},
 	{'!Call Dreadstalkers', 'player.buff(Demonic Calling)'},
 	{'!Hand of Gul\'dan', '!player.moving&player.soulshards>3'},
-	{'!Thal\'kiel\'s Consumption', '!player.moving&player.spell(Call Dreadstalkers).cooldown>3&player.lastgcd(Hand of Gul\'dan)', 'target'},
+	{'!Thal\'kiel\'s Consumption', '!player.moving&player.spell(Call Dreadstalkers).cooldown>3&player.lastgcd(Hand of Gul\'dan)'},
 	{'!Demonic Empowerment', '!player.moving&!player.lastgcd(Demonic Empowerment)&{warlock.empower==0||player.lastgcd(Summon Felguard)||player.lastgcd(Call Dreadstalkers)||player.lastgcd(Hand of Gul\'dan)||player.lastgcd(Summon Darkglare)||player.lastgcd(Summon Doomguard)||player.lastgcd(Grimoire: Felguard)||player.lastgcd(Thal\'kiel\'s Consumption)}'},
-	{'!Doom', '!talent(4,1)&toggle(Doom)&!target.debuff(Doom)'},
+	{'!Doom', '!talent(4,1)&toggle(Doom)&!debuff'},
 	{'!Life Tap', 'player.mana<40&player.health>05&{!player.lastgcd(Summon Felguard)||!player.lastgcd(Call Dreadstalkers)||!player.lastgcd(Hand of Gul\'dan)||!player.lastgcd(Summon Darkglare)||!player.lastgcd(Summon Doomguard)||!player.lastgcd(Grimoire: Felguard)}'},
 	{'!Demonbolt', '!player.moving&talent(7,2)&!player.soulshards==4'},
 	{'!Shadow Bolt', '!player.moving&!talent(7,2)&!player.soulshards==4'},
-	{'&89751', 'target.petrange<9&player.area(8).enemies>2'},
+	{'&89751', 'petrange<9&player.area(8).enemies>2'},
 }
 
 local ST = {
@@ -134,33 +147,36 @@ local ST = {
 	{'!Summon Felguard', '!player.moving&!pet.exists&!talent(6,1)'},
 	{'Call Dreadstalkers', '!player.moving&player.buff(Demonic Calling)'},
 	{'Hand of Gul\'dan', '!player.moving&player.soulshards>3'},
-	{'Thal\'kiel\'s Consumption', '!player.moving&player.spell(Call Dreadstalkers).cooldown>3&player.lastgcd(Hand of Gul\'dan)', 'target'},
+	{'Thal\'kiel\'s Consumption', '!player.moving&player.spell(Call Dreadstalkers).cooldown>3&player.lastgcd(Hand of Gul\'dan)'},
 	{'Demonic Empowerment', '!player.moving&!player.lastgcd(Demonic Empowerment)&{warlock.empower==0||player.lastgcd(Summon Felguard)||player.lastgcd(Call Dreadstalkers)||player.lastgcd(Hand of Gul\'dan)||player.lastgcd(Summon Darkglare)||player.lastgcd(Summon Doomguard)||player.lastgcd(Grimoire: Felguard)||player.lastgcd(Thal\'kiel\'s Consumption)}'},
-	{'Doom', '!talent(4,1)&toggle(Doom)&!target.debuff(Doom)&target.inRanged'},
+	{'Doom', '!talent(4,1)&toggle(Doom)&!debuff&inRanged'},
 	{'Life Tap', 'player.mana<40&player.health>05&{!player.lastgcd(Summon Felguard)||!player.lastgcd(Call Dreadstalkers)||!player.lastgcd(Hand of Gul\'dan)||!player.lastgcd(Summon Darkglare)||!player.lastgcd(Summon Doomguard)||!player.lastgcd(Grimoire: Felguard)}'},
 	{'Demonwrath', 'movingfor>1&player.combat.time>2'},
 	{'Demonbolt', '!player.moving&talent(7,2)&!player.soulshards==4'},
 	{'Shadow Bolt', '!player.moving&!talent(7,2)&!player.soulshards==4'},
-	{'&89751', 'target.petrange<9&player.area(8).enemies>2&pet.exists'},
+	{'&89751', 'petrange<9&player.area(8).enemies>2&pet.exists'},
+}
+
+local xCombat = {
+	{Interrupts, 'toggle(Interrupts)&@Zylla.InterruptAt(intat)&inFront&range<31'},
+	{Interrupts, 'toggle(Interrupts)&toggle(xIntRandom)&@Zylla.InterruptAt(intat)&inFront&range<31', 'enemies'},
+	{Cooldowns, 'toggle(cooldowns)'},
+	{Player, '!player.moving'},
+	{ST, 'inFront&range<31'},
 }
 
 local inCombat = {
 	{Keybinds},
-	{Interrupts_Random},
-	{Interrupts, 'toggle(Interrupts)&target.inFront&target.range<31'},
-	{Survival},
-	{Player, '!player.moving'},
+	{Survival, nil, 'player'},
 	{Mythic_Plus, 'range<31'},
-	{Cooldowns, 'toggle(cooldowns)'},
-	{ST, 'target.inFront&target.range<31'},
 }
 
 local outCombat = {
+	{PreCombat, nil, 'player'},
 	{'Summon Felguard', '{!pet.exists||!pet.alive}&!talent(6,1)}'},
 	{'Life Tap', 'player.mana<70&player.health>50'},
-	{Interrupts_Random},
-	{Interrupts, 'toggle(Interrupts)'},
-	{'Create Healthstone', 'item(5512).count<1&!lastcast(Create Healthstone)'},
+	{'Create Healthstone', 'item(5512).count==0&!lastcast(Create Healthstone)', 'player'},
+	{'Soulstone', 'UI(ss)&!buff', 'player'}
 }
 
 NeP.CR:Add(266, {
