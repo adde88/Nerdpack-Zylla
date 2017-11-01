7d015e060d0b62970d4843acd98cd289bfdf92e6
Zylla <adde88@gmail.com>
2017-11-01 09:23:42 +0100

    Unit updates.

--- fakeunits.lua
+++ fakeunits.lua
@@ -4,101 +4,108 @@ local NeP = _G.NeP
 
 -- Healing stuff
 NeP.Units:Add('healingCandidate', function(nump)
-      local tempTable = {}
+      local tmp = {}
       local num = nump or 1
-      for _, Obj in pairs(NeP.OM:Get('Friendly')) do
-         if _G.UnitPlayerOrPetInParty(Obj.key) or _G.UnitIsUnit('player', Obj.key) then
-            local healthRaw = Zylla.GetPredictedHealth(Obj.key)
-            local maxHealth = _G.UnitHealthMax(Obj.key)
+			for i=1, NeP.Protected.GetObjectCount() do
+				local Obj = NeP.Protected.GetObjectWithIndex(i)
+				if NeP.Protected.omVal(Obj)
+				and _G.UnitIsFriend('player', Obj)
+        and (_G.UnitInRaid(Obj) or _G.UnitInParty(Obj)) then
+            local healthRaw = Zylla.GetPredictedHealth(Obj)
+            local maxHealth = _G.UnitHealthMax(Obj)
             local healthPercent =  (healthRaw / maxHealth) * 100
-            tempTable[#tempTable+1] = {
-               key = Obj.key,
-               health = healthPercent,
+            tmp[#tmp+1] = {
+               key = Obj,
+               prio = healthPercent,
             }
          end
       end
-      table.sort( tempTable, function(a,b) return a.health < b.health end )
-      print("Zylla Unit: " ..tempTable[num].key .." Health: " ..tempTable[num].health)
-      return tempTable[num].key
+      table.sort( tmp, function(a,b) return a.prio < b.prio end )
+      print("Zylla Unit: " ..tmp[num].key .." Health: " ..tmp[num].prio)
+      return tmp
 end)
 
 -- Customized for Windwalker Rotation
 NeP.Units:Add('Zylla_sck', function(debuff)
-      for _, Obj in pairs(NeP.OM:Get('Enemy')) do
-         if _G.UnitExists(Obj.key) then
-            if (NeP.Condition:Get('combat')(Obj.key) or Obj.isdummy) then
-               if (NeP.Condition:Get('infront')(Obj.key) and NeP.Condition:Get('inMelee')(Obj.key)) then
-                  local _,_,_,_,_,_,debuffDuration = _G.UnitDebuff(Obj.key, debuff, nil, 'PLAYER')
-                  if not debuffDuration or debuffDuration - _G.GetTime() < 1.5 then
-                     --print("Zylla_sck: returning "..Obj.name.." ("..Obj.key.." - "..Obj.guid..' :'..time()..")");
-                     return Obj.key
-                  end
-               end
-            end
-         end
-      end
+	for i=1, NeP.Protected.GetObjectCount() do
+		local Obj = NeP.Protected.GetObjectWithIndex(i)
+		if NeP.Protected.omVal(Obj)
+		and NeP.Condition:Get('combat')(Obj)
+		and (NeP.Condition:Get('infront')(Obj)
+		and NeP.Condition:Get('inMelee')(Obj)) then
+			local _,_,_,_,_,_,debuffDuration = _G.UnitDebuff(Obj, debuff, nil, 'PLAYER')
+			if not debuffDuration or debuffDuration - _G.GetTime() < 1.5 then
+				print("Zylla_sck: returning "..Obj.name.." ("..Obj.." - "..Obj.guid..' :'.._G.time()..")");
+				return Obj
+			end
+		end
+	end
 end)
 
 -- Highest Health Enemy
 NeP.Units:Add({'highestenemy', 'higheste', 'he'}, function(num)
-      local tempTable = {}
-      for _, Obj in pairs(NeP.OM:Get('Enemy')) do
-         if _G.UnitExists(Obj.key) then
-            tempTable[#tempTable+1] = {
-               key = Obj.key,
-               health = NeP.Condition:Get("health")(Obj.key)
-            }
-         end
-      end
-      table.sort( tempTable, function(a,b) return a.health > b.health end )
-      return tempTable[num] and tempTable[num].key
+      local tmp = {}
+      for i=1, NeP.Protected.GetObjectCount() do
+				local Obj = NeP.Protected.GetObjectWithIndex(i)
+				if NeP.Protected.omVal(Obj)
+				and _G.UnitCanAttack('player', Obj) then
+					tmp[#tmp+1] = {
+						key = Obj,
+						prio = _G.UnitHealthMax(Obj)
+					}
+				end
+			end
+			table.sort( tmp, function(a,b) return a.prio > b.prio end )
+			return tmp
 end)
 
 -- Feral Druid Stuff         XXX: Remember to set the 'ptf_timer' variable in your UI Settings.
-NeP.Units:Add({'nobleedenemy', 'nobleede'}, function()
-      local ptf_timer    = tonumber(NeP.Condition:Get('UI')(nil, 'ptftimer_spin'))
-      for _, Obj in pairs(NeP.OM:Get('Enemy')) do
-         if _G.UnitExists(Obj.key) then
-            local rip_duration         = tonumber(NeP.Condition:Get('debuff.duration')(Obj.key, 'Rip'))
-            local rake_duration     = tonumber(NeP.Condition:Get('debuff.duration')(Obj.key, 'Rake'))
-            local thrash_duration = tonumber(NeP.Condition:Get('debuff.duration')(Obj.key, 'Thrash'))
-            if (NeP.Condition:Get('inFront')(Obj.key) and NeP.Condition:Get('inMelee')(Obj.key)) and
-            rip_duration         < ptf_timer and
-            rake_duration     < ptf_timer and
-            thrash_duration < ptf_timer then
-               return Obj.key
-            end
-         end
-      end
+	NeP.Units:Add({'nobleedenemy', 'nobleede'}, function()
+      local ptf_timer = tonumber(NeP.Condition:Get('UI')(nil, 'ptftimer_spin'))
+      for i=1, NeP.Protected.GetObjectCount() do
+				local Obj = NeP.Protected.GetObjectWithIndex(i)
+        if NeP.Protected.omVal(Obj) then
+					local rip_duration = tonumber(NeP.Condition:Get('debuff.duration')(Obj, 'Rip'))
+          local rake_duration = tonumber(NeP.Condition:Get('debuff.duration')(Obj, 'Rake'))
+          local thrash_duration = tonumber(NeP.Condition:Get('debuff.duration')(Obj, 'Thrash'))
+          if (NeP.Condition:Get('inFront')(Obj) and NeP.Condition:Get('inMelee')(Obj))
+          and rip_duration < ptf_timer
+          and rake_duration < ptf_timer
+          and thrash_duration < ptf_timer then
+						return Obj
+					end
+				end
+			end
 end)
 
 -- Nearest Enemy
-NeP.Units:Add({'nearestenemy', 'neareste', 'ne'}, function(num)
-      local tempTable = {}
-      for _, Obj in pairs(NeP.OM:Get('Enemy')) do
-         if _G.UnitExists(Obj.key) then
-            tempTable[#tempTable+1] = {
-               key = Obj.key,
-               range = NeP.Condition:Get("range")(Obj.key)
+NeP.Units:Add({'nearestenemy', 'neareste', 'ne'}, function(wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwnum)
+      local tmp = {}
+			for i=1, NeP.Protected.GetObjectCount() do
+				local Obj = NeP.Protected.GetObjectWithIndex(i)
+				if NeP.Protected.omVal(Obj) then
+            tmp[#tmp+1] = {
+               key = Obj,
+               prio = NeP.Condition:Get("range")(Obj)
             }
          end
       end
-      table.sort( tempTable, function(a,b) return a.range < b.range end )
-      return tempTable[num] and tempTable[num].key
+      table.sort( tmp, function(a,b) return a.prio < b.prio end )
+      return tmp
 end)
 
 -- Furthest Enemy (Within 60 yd)
 NeP.Units:Add({'furthestenemy', 'furtheste', 'fe'}, function(num)
-      local tempTable = {}
-      for _, Obj in pairs(NeP.OM:Get('Enemy')) do
-         if _G.UnitExists(Obj.key) and
-				 NeP.Condition:Get("range")(Obj.key) <= 60 then
-            tempTable[#tempTable+1] = {
-               key = Obj.key,
-               range = NeP.Condition:Get("range")(Obj.key)
-            }
-         end
-      end
-      table.sort( tempTable, function(a,b) return a.range > b.range end )
-      return tempTable[num] and tempTable[num].key
+      local tmp = {}
+			for i=1, NeP.Protected.GetObjectCount() do
+				local Obj = NeP.Protected.GetObjectWithIndex(i)
+				if NeP.Protected.omVal(Obj) and NeP.Condition:Get("range")(Obj) <= 60 then
+					tmp[#tmp+1] = {
+						key = Obj,
+						prio = NeP.Condition:Get("range")(Obj)
+					}
+				end
+			end
+			table.sort( tmp, function(a,b) return a.prio > b.prio end )
+			return tmp
 end)
