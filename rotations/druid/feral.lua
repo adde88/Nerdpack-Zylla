9c48226bc13ca9330562466abfd1a5795898612b
Zylla <adde88@gmail.com>
2017-11-23 12:05:28 +0100

    Fix the default target on ALL CR's! SHIT....

--- rotations/druid/feral.lua
+++ rotations/druid/feral.lua
@@ -19,7 +19,7 @@ local GUI = {
 	{type = 'ruler'},	 	{type = 'spacer'},
 	--TODO: Targetting: Use, or NOT use?! We'll see....
 	{type = 'header', 	size = 16, text = 'Targetting:',													align = 'center'},
-	{type = 'combo',		default = 'normal',																				key = 'target', 					list = Zylla.faketarget, 	width = 75},
+	{type = 'combo',		default = 'target',																				key = 'target', 					list = Zylla.faketarget, 	width = 75},
 	{type = 'spacer'},
 	{type = 'text', 		text = Zylla.ClassColor..'Only one can be enabled.\nChose between normal targetting, or hitting the highest/lowest enemy.|r'},
 	{type = 'spacer'},	{type = 'spacer'},
