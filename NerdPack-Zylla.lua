8b624a30b51904277a311896c39cbc05aa5d5862
Zylla <adde88@gmail.com>
2017-11-23 14:42:19 +0100

    Version Bump

--- NerdPack-Zylla.lua
+++ NerdPack-Zylla.lua
@@ -3,7 +3,7 @@ local _G = _G
 local NeP = _G.NeP
 _G.Zylla = Zylla
 
-Zylla.Version = '2.6'
+Zylla.Version = '2.7'
 Zylla.Branch = 'RELEASE'
 Zylla.Name = 'NerdPack - Zylla\'s Rotations'
 Zylla.Author = 'Zylla'
@@ -14,7 +14,8 @@ Zylla.nep_ver = '1.11'
 Zylla.timer = {}
 Zylla.spell_timers = {}
 Zylla.isAFK = false;
-Zylla.Class = select(3,_G.UnitClass("player"))
+Zylla.Class = select(3, _G.UnitClass("player"))
+Zylla.Spec = GetInspectSpecialization('player')
 Zylla.DonateURL = 'https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=23HX4QKDAD4YG'
 Zylla.GuiSettings = {title='Zylla\'s Combat Routines', width='256', height='960', color='A330C9'}
 Zylla.PetMode = 0
