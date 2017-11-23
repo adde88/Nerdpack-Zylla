df12fd18aef52a375677b51cb0e928c03d6797bb
Zylla <adde88@gmail.com>
2017-11-23 15:48:54 +0100

    Dirty fix for GetInspectSpecialization() sometimes returning 0.

--- NerdPack-Zylla.lua
+++ NerdPack-Zylla.lua
@@ -15,7 +15,7 @@ Zylla.timer = {}
 Zylla.spell_timers = {}
 Zylla.isAFK = false;
 Zylla.Class = select(3, _G.UnitClass("player"))
-Zylla.Spec = GetInspectSpecialization('player')
+Zylla.Spec = _G.GetInspectSpecialization('player')
 Zylla.DonateURL = 'https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=23HX4QKDAD4YG'
 Zylla.GuiSettings = {title='Zylla\'s Combat Routines', width='256', height='960', color='A330C9'}
 Zylla.PetMode = 0
@@ -25,3 +25,10 @@ Zylla.GuiSettings = {
   height='960',
   color='A330C9'
 }
+
+_G.C_Timer.NewTicker(1, function()
+	local specid = Zylla.Spec
+	if specid == 0 then
+		Zylla.Spec = _G.GetInspectSpecialization('player')
+	end
+end)
