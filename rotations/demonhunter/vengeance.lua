295cfdd94a047f33a43bd926228ab242f92d2cca
Zylla <adde88@gmail.com>
2017-11-25 16:00:37 +0100

    Improved for lvling. NeP tries to use those spells which the player doesn't have when fresh level 100.

--- rotations/demonhunter/vengeance.lua
+++ rotations/demonhunter/vengeance.lua
@@ -118,7 +118,7 @@ local Cooldowns = {
 
 local Mitigations = {
 	{'!Metamorphosis', 'toggle(cooldowns)&UI(meta_check)&!buff(Demon Spikes)&!target.debuff(Fiery Brand)&!buff&{{health<75&incdmg(1)>=health.max*0.50}||health<=UI(meta_spin)}'},
-	{'!Demon Spikes', 'player.pain>21&spell.charges>0&!buff&!target.debuff(Fiery Brand)&!buff(Metamorphosis)'},
+	{'!Demon Spikes', 'spell.exists&player.pain>21&spell.charges>0&!buff&!target.debuff(Fiery Brand)&!buff(Metamorphosis)'},
 	{'!Empower Wards', 'incdmg(3).magic>player.health.max*0.1&health<75'},
 	{'!Soul Barrier', 'buff(Soul Fragments).count>UI(sb_spin)&UI(sb_check)&health<=UI(sbhp)'},
 	{'Immolation Aura', 'area(8).enemies>0'},
@@ -128,7 +128,7 @@ local xCombat = {
 	{Cooldowns},
 	{Interrupts, '@Zylla.InterruptAt(intat)&toggle(Interrupts)'},
 	{Interrupts, '@Zylla.InterruptAt(intat)&toggle(Interrupts)&toggle(xIntRandom)', 'enemies'},
-	{'Fiery Brand', 'inFront&inMelee&!player.buff(Demon Spikes)&!player.buff(Metamorphosis)'},
+	{'Fiery Brand', 'spell.exists&inFront&inMelee&!player.buff(Demon Spikes)&!player.buff(Metamorphosis)'},
 	{'Soul Carver', 'inFront&inMelee&{spell(Fiery Brand).cooldown>50||player.incdmg(3)>player.health.max*0.2}'},
 	{'Shear', 'inFront&inMelee&player.pain<90'},
 	{'Sever', 'inFront&inMelee'},
