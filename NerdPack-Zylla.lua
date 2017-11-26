28ec23f026513e12ed08fe4173f7765be9198fc7
Zylla <adde88@gmail.com>
2017-11-26 10:53:56 +0100

    Bump dev. versionn

--- NerdPack-Zylla.lua
+++ NerdPack-Zylla.lua
@@ -3,7 +3,7 @@ local _G = _G
 local NeP = _G.NeP
 _G.Zylla = Zylla
 
-Zylla.Version = '2.7'
+Zylla.Version = '2.8'
 Zylla.Branch = 'DEV'
 Zylla.Name = 'NerdPack - Zylla\'s Rotations'
 Zylla.Author = 'Zylla'
